eeb429759bff9eb6a7d4fc76a2c743d62f40dda4
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-04 09:33:27 +0500

    UPD

---
+++ RemoteMaster.Client/FluentValidators/ComputerValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RemoteMaster.Client.Models;
+
+namespace RemoteMaster.Client.FluentValidators;
+
+public class ComputerValidator : AbstractValidator<Computer>
+{
+    public ComputerValidator()
+    {
+        RuleFor(vm => vm.Name)
+            .NotEmpty()
+            .MaximumLength(20);
+
+        RuleFor(vm => vm.IPAddress)
+            .NotEmpty();
+    }
+}
