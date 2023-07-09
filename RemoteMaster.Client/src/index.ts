361b2db2494754056ff4d8ae019a1097ef07456a
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-09 07:59:56 +0500

    UPD (TS)

---
+++ RemoteMaster.Client/src/index.ts
@@ -0,0 +1,25 @@
+﻿import * as signalR from "@microsoft/signalr";
+import { MessagePackHubProtocol } from "@microsoft/signalr-protocol-msgpack";
+
+declare global {
+    interface Window {
+        setupSignalRConnection: (host: string) => void;
+    }
+}
+
+window.setupSignalRConnection = function (host: string) {
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(`http://${host}:5076/hubs/control`, {
+            skipNegotiation: true,
+            transport: signalR.HttpTransportType.WebSockets
+        })
+        .withHubProtocol(new MessagePackHubProtocol())
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("ScreenUpdate", (data) => {
+        console.log(data);
+    });
+
+    connection.start().catch(err => console.error(err.toString()));
+}
