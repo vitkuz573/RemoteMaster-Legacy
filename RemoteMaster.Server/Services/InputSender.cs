f8c25866ee3b6559a540e16e2c956870a159ea78
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-11 21:49:43 +0500

    UPD

--- RemoteMaster.Server/Services/InputSender.cs
+++ RemoteMaster.Server/Services/InputSender.cs
@@ -1,4 +1,5 @@
 ﻿using RemoteMaster.Server.Abstractions;
+using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
 using static Windows.Win32.PInvoke;
@@ -8,73 +9,115 @@ namespace RemoteMaster.Server.Services;
 public class InputSender : IInputSender
 {
     private readonly ILogger<InputSender> _logger;
+    private readonly ConcurrentQueue<Action> _operationQueue;
+    private Thread _workerThread;
+    private CancellationTokenSource _cts;
 
     public InputSender(ILogger<InputSender> logger)
     {
         _logger = logger;
+        _operationQueue = new ConcurrentQueue<Action>();
+        _cts = new CancellationTokenSource();
+        _workerThread = new Thread(() => ProcessQueue(_cts.Token)) { IsBackground = true };
+        _workerThread.Start();
+    }
+
+    public void EnqueueOperation(Action operation)
+    {
+        _operationQueue.Enqueue(operation);
+    }
+
+    private void ProcessQueue(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            if (_operationQueue.TryDequeue(out var operation))
+            {
+                operation();
+                Thread.Sleep(10);
+            }
+            else
+            {
+                Thread.Sleep(50);
+            }
+        }
+    }
+
+    public void StopProcessing()
+    {
+        _cts.Cancel();
+        _workerThread.Join();
+        _workerThread = null;
+        _cts = null;
     }
 
     public void SendMouseCoordinates(int x, int y)
     {
-        var input = new INPUT
+        EnqueueOperation(() =>
         {
-            type = INPUT_TYPE.INPUT_MOUSE,
-            Anonymous =
+            var input = new INPUT
             {
-                mi = new MOUSEINPUT
+                type = INPUT_TYPE.INPUT_MOUSE,
+                Anonymous =
                 {
-                    dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_VIRTUALDESK,
-                    dx = x,
-                    dy = y,
-                    time = 0,
-                    mouseData = 0,
-                    dwExtraInfo = (nuint)GetMessageExtraInfo().Value
+                    mi = new MOUSEINPUT
+                    {
+                        dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_VIRTUALDESK,
+                        dx = x,
+                        dy = y,
+                        time = 0,
+                        mouseData = 0,
+                        dwExtraInfo = (nuint)GetMessageExtraInfo().Value
+                    }
                 }
-            }
-        };
+            };
 
-        var inputs = new Span<INPUT>(ref input);
+            var inputs = new Span<INPUT>(ref input);
 
-        SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+            SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+        });
     }
 
     public void SendMouseButton(long button, string state, int x, int y)
     {
-        var mouseEvent = button switch
+        EnqueueOperation(() =>
         {
-            0 => state switch
+            var mouseEvent = button switch
             {
-                "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN,
-                "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTUP
-            },
-            1 => state switch
-            {
-                "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEDOWN,
-                "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEUP
-            },
-            2 => state switch
-            {
-                "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN,
-                "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP
-            }
-        };
-
-        var input = new INPUT
-        {
-            type = INPUT_TYPE.INPUT_MOUSE,
-            Anonymous =
-            {
-                mi = new MOUSEINPUT
+                0 => state switch
                 {
-                    dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE | mouseEvent | MOUSE_EVENT_FLAGS.MOUSEEVENTF_VIRTUALDESK,
-                    dx = x,
-                    dy = y
+                    "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN,
+                    "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTUP
+                },
+                1 => state switch
+                {
+                    "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEDOWN,
+                    "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEUP
+                },
+                2 => state switch
+                {
+                    "mousedown" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN,
+                    "mouseup" => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP
                 }
-            }
-        };
+            };
 
-        var inputs = new Span<INPUT>(ref input);
+            var input = new INPUT
+            {
+                type = INPUT_TYPE.INPUT_MOUSE,
+                Anonymous =
+                {
+                    mi = new MOUSEINPUT
+                    {
+                        dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE | mouseEvent | MOUSE_EVENT_FLAGS.MOUSEEVENTF_VIRTUALDESK,
+                        dx = x,
+                        dy = y
+                    }
+                }
+            };
 
-        SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+            var inputs = new Span<INPUT>(ref input);
+
+            SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+        });
     }
 }
