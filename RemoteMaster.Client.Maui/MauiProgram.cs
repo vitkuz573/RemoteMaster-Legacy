c3379de4df9b0fd24c1ba22958b9d77007f861ab
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-17 14:26:14 +0500

    UPD

--- RemoteMaster.Client.Maui/MauiProgram.cs
+++ RemoteMaster.Client.Maui/MauiProgram.cs
@@ -1,30 +1,40 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RemoteMaster.Client.Maui.Data;
+using RemoteMaster.Client.Maui.Services;
 
-namespace RemoteMaster.Client.Maui
+namespace RemoteMaster.Client.Maui;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
+
+        builder.Services.AddScoped<DatabaseService>();
+        builder.Services.AddScoped<ActiveDirectoryService>();
+        builder.Services.AddTransient<IHubConnectionBuilder>(s => new HubConnectionBuilder());
+        builder.Services.AddValidatorsFromAssembly(typeof(App).Assembly);
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
