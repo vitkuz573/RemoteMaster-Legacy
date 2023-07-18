6be9468c27f379ee672b48af70d8b454ba5c91d2
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-18 07:31:13 +0500

    UPD

---
+++ RemoteMaster.Client/Pages/AddComputerModal.razor.cs
@@ -0,0 +1,52 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using RemoteMaster.Client.Models;
+using System.Collections.ObjectModel;
+
+namespace RemoteMaster.Client.Pages;
+
+public partial class AddComputerModal
+{
+    public Modal _modalRef;
+    public Computer _newComputer = new();
+    public Guid? _selectedFolderId;
+
+    private Validations _fluentValidations;
+
+    public void Show()
+    {
+        _modalRef.Show();
+    }
+
+    public void Hide()
+    {
+        _modalRef.Hide();
+    }
+
+    public async void AddComputer()
+    {
+        if (await _fluentValidations.ValidateAll())
+        {
+            var folder = Nodes.OfType<Folder>().FirstOrDefault(f => f.NodeId == _selectedFolderId);
+
+            if (folder != null)
+            {
+                _newComputer.ParentId = folder.NodeId;
+                folder.Children.Add(_newComputer);
+            }
+
+            _newComputer = new Computer();
+            _selectedFolderId = null;
+            Hide();
+        }
+    }
+
+    private void OnSelectedFolderChanged(Guid? selectedId)
+    {
+        Console.WriteLine($"Selected folder ID changed to: {selectedId}");
+        _selectedFolderId = selectedId;
+    }
+
+    [Parameter]
+    public ObservableCollection<Node> Nodes { get; set; }
+}
