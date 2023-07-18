b5abd4fadb5a7b4dd8e6e9b63d66c40970cb8f4f
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-18 07:29:16 +0500

    UPD

---
+++ RemoteMaster.Client/Pages/AddFolderModal.razor.cs
@@ -0,0 +1,36 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using RemoteMaster.Client.Models;
+
+namespace RemoteMaster.Client.Pages;
+
+public partial class AddFolderModal
+{
+    private Modal _modalRef;
+    private Folder _newFolder = new();
+
+    private Validations _fluentValidations;
+
+    public void Show()
+    {
+        _modalRef.Show();
+    }
+
+    public void Hide()
+    {
+        _modalRef.Hide();
+    }
+
+    public async void AddFolder()
+    {
+        if (await _fluentValidations.ValidateAll())
+        {
+            await OnAdd.InvokeAsync(_newFolder);
+            _newFolder = new Folder();
+            Hide();
+        }
+    }
+
+    [Parameter]
+    public EventCallback<Folder> OnAdd { get; set; }
+}
