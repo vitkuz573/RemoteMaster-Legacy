d980ca908e0c303b88beca54c3b082d24364ff51
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-07-18 07:26:04 +0500

    UPD

---
+++ RemoteMaster.Client/Pages/SyncResultsModal.razor.cs
@@ -0,0 +1,24 @@
+﻿using Blazorise;
+using RemoteMaster.Client.Models;
+using System.Collections.ObjectModel;
+
+namespace RemoteMaster.Client.Pages;
+
+public partial class SyncResultsModal
+{
+    private Modal _modalRef;
+    private ObservableCollection<Node> _adNodes = new();
+    private IList<Node> _expandedNodes = new List<Node>();
+    private Node _selectedNode;
+
+    public void Show(ObservableCollection<Node> adNodes)
+    {
+        _adNodes = adNodes;
+        _modalRef.Show();
+    }
+
+    public void Hide()
+    {
+        _modalRef.Hide();
+    }
+}
