7baad99cf8ae5a484b5c39e51e77dd7578c27a78
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-08-03 15:52:25 +0500

    Обновил заголовки

--- RemoteMaster.Shared/FileLoggerProvider.cs
+++ RemoteMaster.Shared/FileLoggerProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
-// Unauthorized copying of this file, via any medium is strictly prohibited.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
 
 using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
