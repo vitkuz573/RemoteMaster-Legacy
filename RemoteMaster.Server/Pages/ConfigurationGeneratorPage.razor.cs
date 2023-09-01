a9f9688dde0f0a96c05a5123ce73d48e2d2a4862
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-01 10:02:35 +0500

    UPD

---
+++ RemoteMaster.Server/Pages/ConfigurationGeneratorPage.razor.cs
@@ -0,0 +1,42 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using RemoteMaster.Server.Abstractions;
+using RemoteMaster.Server.Models;
+
+namespace RemoteMaster.Server.Pages;
+
+public partial class ConfigurationGeneratorPage
+{
+    private bool isConfigGenerated = false;
+
+    private readonly IConfiguratorService _configuratorService;
+    private readonly ILogger<ConfigurationGeneratorPage> _logger;
+
+    public ConfigurationGeneratorPage(IConfiguratorService configuratorPage, ILogger<ConfigurationGeneratorPage> logger)
+    {
+        _configuratorService = configuratorPage;
+        _logger = logger;
+    }
+
+    private async Task GenerateConfig()
+    {
+        try
+        {
+            var config = new ConfigurationModel
+            {
+                ServerUrl = "http://example.com",
+                ClientId = Guid.NewGuid().ToString()
+            };
+
+            await _configuratorService.GenerateConfigFileAsync("C:/users/vitaly/Desktop/host.json", config);
+            isConfigGenerated = true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while generating the config.");
+            isConfigGenerated = false;
+        }
+    }
+}
---
+++ RemoteMaster.Server/Services/ConfiguratorService.cs
@@ -0,0 +1,43 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RemoteMaster.Server.Abstractions;
+using RemoteMaster.Server.Models;
+
+namespace RemoteMaster.Server.Services;
+
+public class ConfiguratorService : IConfiguratorService
+{
+    private readonly JsonSerializerOptions _jsonOptions;
+    private readonly ILogger<ConfiguratorService> _logger;
+
+    public ConfiguratorService(ILogger<ConfiguratorService> logger)
+    {
+        _logger = logger;
+
+        _jsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+    }
+
+    public async Task GenerateConfigFileAsync(string path, ConfigurationModel config)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(config, _jsonOptions);
+            await File.WriteAllTextAsync(path, json);
+            _logger.LogInformation($"Successfully generated config file at {path}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while generating config file.");
+            throw;
+        }
+    }
+}
