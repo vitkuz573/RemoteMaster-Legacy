ff01259c7dc22a63a348aab6d18a3a531481d9dd
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-01 14:01:42 +0500

    UPDATE!!!!!!!!!!!!!!!!!!!!!!

--- RemoteMaster.Server/Hubs/ManagementHub.cs
+++ RemoteMaster.Server/Hubs/ManagementHub.cs
@@ -3,14 +3,38 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using Microsoft.AspNetCore.SignalR;
+using RemoteMaster.Server.Models;
+using RemoteMaster.Server.Services;
 
 namespace RemoteMaster.Server.Hubs;
 
 public class ManagementHub : Hub
 {
+    private readonly DatabaseService _databaseService;
+
+    public ManagementHub(DatabaseService databaseService)
+    {
+        _databaseService = databaseService;
+    }
+
     public async Task<bool> RegisterClient(string hostName, string ipAddress, string group)
     {
-        // Ваша логика здесь
+        var folder = _databaseService.GetFolders().FirstOrDefault(f => f.Name == group);
+
+        if (folder == null)
+        {
+            folder = new Folder(group);
+            _databaseService.AddNode(folder);
+        }
+
+        var computer = new Computer
+        {
+            Name = hostName,
+            IPAddress = ipAddress,
+            Parent = folder
+        };
+
+        _databaseService.AddNode(computer);
 
         return true;
     }
