a8439740c90a7006782782d3a4f3e88dff1ef415
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-01 15:00:04 +0500

    Обновляю данные при повторном добавлении чтобы избежать дублирования

--- RemoteMaster.Server/Hubs/ManagementHub.cs
+++ RemoteMaster.Server/Hubs/ManagementHub.cs
@@ -1,8 +1,4 @@
-﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
-// This file is part of the RemoteMaster project.
-// Licensed under the GNU Affero General Public License v3.0.
-
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using RemoteMaster.Server.Models;
 using RemoteMaster.Server.Services;
 
@@ -20,21 +16,30 @@ public class ManagementHub : Hub
     public async Task<bool> RegisterClient(string hostName, string ipAddress, string group)
     {
         var folder = _databaseService.GetFolders().FirstOrDefault(f => f.Name == group);
-
+        
         if (folder == null)
         {
             folder = new Folder(group);
             _databaseService.AddNode(folder);
         }
 
-        var computer = new Computer
-        {
-            Name = hostName,
-            IPAddress = ipAddress,
-            Parent = folder
-        };
+        var existingComputer = _databaseService.GetComputersByFolderId(folder.NodeId).FirstOrDefault(c => c.Name == hostName);
 
-        _databaseService.AddNode(computer);
+        if (existingComputer != null)
+        {
+            existingComputer.IPAddress = ipAddress;
+        }
+        else
+        {
+            var computer = new Computer
+            {
+                Name = hostName,
+                IPAddress = ipAddress,
+                Parent = folder
+            };
+
+            _databaseService.AddNode(computer);
+        }
 
         return true;
     }
