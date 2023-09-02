01994c6bb42d1e4d74b5f43f324c1d73a8fdef84
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-02 16:53:39 +0500

    UPDDD

---
+++ RemoteMaster.Agent/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RemoteMaster.Agent;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ RemoteMaster.Agent/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Text.Json;
+using System.Windows;
+using RemoteMaster.Shared.Models;
+
+namespace RemoteMaster.Agent;
+
+public partial class MainWindow : Window
+{
+    private const string MainAppName = "RemoteMaster";
+    private const string SubAppName = "Agent";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        LoadConfigurationFromFile();
+    }
+
+    private void LoadConfigurationFromFile()
+    {
+        var fileName = $"{AppDomain.CurrentDomain.FriendlyName}.json";
+
+        if (File.Exists(fileName))
+        {
+            using var reader = new StreamReader(fileName);
+            var json = reader.ReadToEnd();
+
+            try
+            {
+                var config = JsonSerializer.Deserialize<ConfigurationModel>(json);
+
+                if (config == null || string.IsNullOrWhiteSpace(config.Server) || string.IsNullOrWhiteSpace(config.Group))
+                {
+                    ShowErrorAndExit("Configuration is invalid. The application will now exit.");
+                }
+                else
+                {
+                    ServerAddressTextBlock.Text = $"Server Address: {config.Server}";
+                    GroupTextBlock.Text = $"PC Group: {config.Group}";
+                }
+            }
+            catch (JsonException)
+            {
+                ShowErrorAndExit("Error parsing the configuration file. The application will now exit.");
+            }
+        }
+        else
+        {
+            ShowErrorAndExit("Configuration file not found. The application will now exit.");
+        }
+    }
+
+    private static void ShowErrorAndExit(string message)
+    {
+        MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        Application.Current.Shutdown();
+    }
+
+    private void InstallUpdateButton_Click(object sender, RoutedEventArgs e)
+    {
+        InstallWindowsService("RCService");
+    }
+
+    private static void InstallWindowsService(string serviceName)
+    {
+        var currentExecutablePath = Process.GetCurrentProcess().MainModule.FileName;
+        var programFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        var newExecutablePath = Path.Combine(programFilesPath, MainAppName, $"{MainAppName}.{SubAppName}.exe");
+
+        if (!File.Exists(newExecutablePath))
+        {
+            File.Copy(currentExecutablePath, newExecutablePath);
+        }
+
+        using var serviceController = new ServiceController(serviceName);
+
+        if (ServiceController.GetServices().Any(s => s.ServiceName == serviceName))
+        {
+            return;
+        }
+
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "sc",
+            Arguments = $"create {serviceName} binPath= \"{newExecutablePath}\" start= auto",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            Verb = "runas"
+        };
+
+        using var process = new Process { StartInfo = processStartInfo };
+        process.Start();
+        process.WaitForExit();
+    }
+
+    private void UninstallButton_Click(object sender, RoutedEventArgs e) { }
+}
