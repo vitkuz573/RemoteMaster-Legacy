55a5167e8fc72b469c47b624a785e67ce0be522d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-06 10:22:18 +0500

    UPD

---
+++ scripts/certgen.ps1
@@ -0,0 +1,7 @@
+$tenYearsFromNow = (Get-Date).AddYears(10)
+$cert = New-SelfSignedCertificate -Subject "CN=RemoteMaster Development" -Type CodeSigning -CertStoreLocation cert:\CurrentUser\My -NotAfter $tenYearsFromNow
+$tempFilePath = Join-Path $env:TEMP "tempCert.cer"
+Export-Certificate -Cert $cert -FilePath $tempFilePath
+Import-Certificate -FilePath $tempFilePath -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+Import-Certificate -FilePath $tempFilePath -CertStoreLocation Cert:\LocalMachine\Root
+Remove-Item -Path $tempFilePath -Force
