1e5a5aee9a68d3358096482fb0c99abf20c144da
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-07 13:57:33 +0500

    Автоматическая подпись при публикации

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -1,2 +1,6 @@
+$certSubject = "CN=RemoteMaster Development"
+$certStoreLocation = "Cert:\CurrentUser\My"
+$cert = Get-ChildItem $certStoreLocation | Where-Object { $_.Subject -eq $certSubject }
+
 Set-AuthenticodeSignature "C:\Program Files\RemoteMaster\Client\RemoteMaster.Client.exe" -Certificate $cert
 Get-AuthenticodeSignature -FilePath "C:\Program Files\RemoteMaster\Client\RemoteMaster.Client.exe"
