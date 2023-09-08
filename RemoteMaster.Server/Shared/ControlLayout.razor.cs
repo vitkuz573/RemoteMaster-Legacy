1dfc55b427ef6f7fbe6fb74216440118fe773228
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-08 10:35:06 +0500

    Рефакторинг ControlLayout

---
+++ RemoteMaster.Server/Shared/ControlLayout.razor.cs
@@ -0,0 +1,87 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using RemoteMaster.Server.Services;
+
+namespace RemoteMaster.Server.Shared;
+
+public partial class ControlLayout
+{
+    private bool _isMenuOpen = false;
+    private string _clientVersion;
+    private bool _inputEnabled;
+    private bool _cursorTracking;
+    private int _quality;
+
+    private string _rebootMessage = "test";
+    private int _timeout = 60;
+
+    private bool _firstToggleMenu = true;
+
+    [Inject]
+    private ControlFunctionsService ControlFunctionsService { get; set; }
+
+    private void ToggleMenu()
+    {
+        _isMenuOpen = !_isMenuOpen;
+
+        if (_firstToggleMenu)
+        {
+            var clientConfiguration = ControlFunctionsService.ClientConfiguration;
+
+            _clientVersion = clientConfiguration.AppVersion;
+            _inputEnabled = clientConfiguration.InputEnabled;
+            _cursorTracking = clientConfiguration.TrackCursor;
+            _quality = clientConfiguration.ImageQuality;
+
+            _firstToggleMenu = false;
+        }
+    }
+
+    private async void OnChangeScreen(ChangeEventArgs e)
+    {
+        await ControlFunctionsService.ControlHubProxy.SendSelectedScreen(e.Value.ToString());
+    }
+
+    private async void ChangeQuality(int quality)
+    {
+        _quality = quality;
+
+        await ControlFunctionsService.ControlHubProxy.SetQuality(quality);
+    }
+
+    private async Task ToggleCursorTracking(bool value)
+    {
+        _cursorTracking = value;
+
+        await ControlFunctionsService.ControlHubProxy.SetTrackCursor(value);
+    }
+
+    private async Task ToggleInputEnabled(bool value)
+    {
+        _inputEnabled = value;
+
+        await ControlFunctionsService.ControlHubProxy.SetInputEnabled(value);
+    }
+
+    private async void KillClient()
+    {
+        await ControlFunctionsService.ControlHubProxy.KillClient();
+    }
+
+    private async void RebootComputer()
+    {
+        await ControlFunctionsService.ControlHubProxy.RebootComputer(_rebootMessage, _timeout, true);
+    }
+
+    private async void SendCtrlAltDel()
+    {
+        await ControlFunctionsService.ControlHubProxy.SendCtrlAltDel();
+    }
+
+    private static async void UpdateClient()
+    {
+    }
+}
