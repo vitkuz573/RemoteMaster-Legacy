cd76ebb9265ee3554edf0071e70eaedf3f1e768e
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-09 11:03:04 +0500

    Добавил HardwareHelper для управления аппаратной частью удаленного ПК

---
+++ RemoteMaster.Shared/Native/Windows/HardwareHelper.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Runtime.Versioning;
+using Windows.Win32.Foundation;
+using static Windows.Win32.PInvoke;
+
+namespace RemoteMaster.Shared.Native.Windows;
+
+[SupportedOSPlatform("windows6.0.6000")]
+public static class HardwareHelper
+{
+    public enum MonitorState
+    {
+        On = -1,
+        Standby = 1,
+        Off = 2
+    }
+
+    public static void SetMonitorState(MonitorState state)
+    {
+        SendMessage((HWND)0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, state);
+    }
+}
