192dc760608809ebe8b9bcb80989c1b5958eb7c2
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-10 21:18:27 +0500

    UPD

--- RemoteMaster.Shared/Result.cs
+++ RemoteMaster.Shared/Result.cs
@@ -1,4 +1,8 @@
-﻿namespace RemoteMaster.Shared;
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+namespace RemoteMaster.Shared;
 
 /// <summary>
 /// Interface for operation result.
@@ -6,11 +10,8 @@
 public interface IResult
 {
     bool IsSuccess { get; }
-
     bool IsFailure { get; }
-
     IReadOnlyCollection<string> ErrorMessages { get; }
-
     Exception? Exception { get; }
 }
 
@@ -20,11 +21,8 @@ public interface IResult
 public class Result : IResult
 {
     public bool IsSuccess { get; protected set; }
-
     public IReadOnlyCollection<string> ErrorMessages { get; protected set; } = Array.Empty<string>();
-
     public Exception? Exception { get; protected set; }
-
     public bool IsFailure => !IsSuccess;
 
     protected Result(bool isSuccess, IEnumerable<string> errorMessages = null, Exception ex = null)
@@ -54,37 +52,62 @@ public class Result : IResult
     /// <summary>
     /// Executes an action if the result is successful.
     /// </summary>
-    public Result OnSuccess(Action action)
+    public Result OnSuccess(Func<Result> func)
     {
-        if (action == null)
+        if (func == null)
         {
-            throw new ArgumentNullException(nameof(action));
+            throw new ArgumentNullException(nameof(func));
         }
 
-        if (IsSuccess)
-        {
-            action();
-        }
-
-        return this;
+        return IsSuccess ? func() : this;
     }
 
     /// <summary>
     /// Executes an action if the result is a failure.
     /// </summary>
-    public Result OnFailure(Action<IEnumerable<string>, Exception> action)
+    public Result OnFailure(Func<Result> func)
+    {
+        if (func == null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
+        return IsFailure ? func() : this;
+    }
+
+    /// <summary>
+    /// Inspects the result without changing it.
+    /// </summary>
+    public Result Tap(Action<Result> action)
     {
         if (action == null)
         {
             throw new ArgumentNullException(nameof(action));
         }
 
-        if (IsFailure)
-        {
-            action(ErrorMessages, Exception);
-        }
+        action(this);
 
         return this;
+    }
+
+    /// <summary>
+    /// Tries to execute a function and wraps the result or exception in a Result.
+    /// </summary>
+    public static Result Try(Func<Result> func, Func<Exception, string> errorHandler = null)
+    {
+        if (func == null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
+        try
+        {
+            return func();
+        }
+        catch (Exception ex)
+        {
+            return Failure(errorHandler?.Invoke(ex) ?? ex.Message, ex);
+        }
     }
 }
 
@@ -94,13 +117,9 @@ public class Result : IResult
 public class Result<T> : IResult
 {
     public T Value { get; private set; }
-
     public bool IsSuccess { get; private set; }
-
     public IReadOnlyCollection<string> ErrorMessages { get; private set; } = Array.Empty<string>();
-
     public Exception? Exception { get; private set; }
-
     public bool IsFailure => !IsSuccess;
 
     private Result(bool isSuccess, T value = default, IEnumerable<string> errorMessages = null, Exception ex = null)
@@ -129,50 +148,76 @@ public class Result<T> : IResult
         => new Result<T>(false, default, errorMessages, ex);
 
     /// <summary>
-    /// Transforms the result value if successful.
+    /// Executes an action if the result is successful.
     /// </summary>
-    public Result<TOut> Map<TOut>(Func<T, TOut> transform)
+    public Result<T> OnSuccess(Func<T, Result<T>> func)
     {
-        if (transform == null)
+        if (func == null)
         {
-            throw new ArgumentNullException(nameof(transform));
+            throw new ArgumentNullException(nameof(func));
         }
 
-        return IsFailure
-            ? Result<TOut>.Failure(ErrorMessages, Exception)
-            : Result<TOut>.Success(transform(Value));
+        return IsSuccess ? func(Value) : this;
     }
 
     /// <summary>
-    /// Chains the result with a new operation if successful.
+    /// Executes an action if the result is a failure.
     /// </summary>
-    public Result<TOut> AndThen<TOut>(Func<T, Result<TOut>> next)
+    public Result<T> OnFailure(Func<IReadOnlyCollection<string>, Exception, Result<T>> func)
     {
-        if (next == null)
+        if (func == null)
         {
-            throw new ArgumentNullException(nameof(next));
+            throw new ArgumentNullException(nameof(func));
         }
 
-        return IsSuccess
-            ? next(Value)
-            : Result<TOut>.Failure(ErrorMessages, Exception);
+        return IsFailure ? func(ErrorMessages, Exception) : this;
     }
 
     /// <summary>
-    /// Combines multiple results into a single result.
+    /// Inspects the result without changing it.
     /// </summary>
-    public static Result<IEnumerable<T>> Combine(params Result<T>[] results)
+    public Result<T> Tap(Action<Result<T>> action)
     {
-        var failedResults = results.Where(r => r.IsFailure).ToList();
+        if (action == null)
+        {
+            throw new ArgumentNullException(nameof(action));
+        }
 
-        if (failedResults.Any())
+        action(this);
+
+        return this;
+    }
+
+    /// <summary>
+    /// Tries to execute a function and wraps the result or exception in a Result.
+    /// </summary>
+    public static Result<T> Try(Func<Result<T>> func, Func<Exception, string> errorHandler = null)
+    {
+        if (func == null)
         {
-            var allErrors = failedResults.SelectMany(r => r.ErrorMessages);
-            return Result<IEnumerable<T>>.Failure(allErrors);
+            throw new ArgumentNullException(nameof(func));
         }
-        else
+
+        try
         {
-            return Result<IEnumerable<T>>.Success(results.Select(r => r.Value));
+            return func();
         }
+        catch (Exception ex)
+        {
+            return Failure(errorHandler?.Invoke(ex) ?? ex.Message, ex);
+        }
+    }
+
+    /// <summary>
+    /// Transforms the value of this result to another type.
+    /// </summary>
+    public Result<U> Convert<U>(Func<T, U> converter)
+    {
+        if (converter == null)
+        {
+            throw new ArgumentNullException(nameof(converter));
+        }
+
+        return IsSuccess ? Result<U>.Success(converter(Value)) : Result<U>.Failure(ErrorMessages, Exception);
     }
 }
