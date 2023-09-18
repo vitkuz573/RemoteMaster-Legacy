511d76a5b4b8836d0fb2c4d52a15d117a08c059d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-18 19:00:21 +0500

    Начальная реализация защиты сервера

--- RemoteMaster.Server/Program.cs
+++ RemoteMaster.Server/Program.cs
@@ -1,4 +1,4 @@
-// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// Copyright ï¿½ 2023 Vitaly Kuzyaev. All rights reserved.
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
@@ -8,10 +8,18 @@ using Radzen;
 using RemoteMaster.Server.Abstractions;
 using RemoteMaster.Server.Hubs;
 using RemoteMaster.Server.Services;
+using Microsoft.AspNetCore.Identity;
+using RemoteMaster.Server.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/Identity/Account/Login";
+    options.LoginPath = "/Identity/Account/Login";
+});
+
 builder.Services.AddHttpClient("DefaultClient", client =>
 {
     client.BaseAddress = new Uri("http://127.0.0.1:5254");
@@ -25,6 +33,9 @@ builder.Services.AddScoped<DatabaseService>();
 builder.Services.AddScoped<IQueryParameterService, QueryParameterService>();
 builder.Services.AddTransient<IHubConnectionBuilder>(s => new HubConnectionBuilder());
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<IdentityDataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityDataContext>();
 
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
@@ -50,6 +61,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.MapBlazorHub();
