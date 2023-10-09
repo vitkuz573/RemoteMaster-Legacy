f64d7936b0c46cf6e5db83026b45b27cc82724d6
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-10-09 09:18:02 +0500

    Обновил компонент LoginDisplay

---
+++ RemoteMaster.Server/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+
+namespace RemoteMaster.Server.Shared;
+
+public partial class LoginDisplay
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private void Logout()
+    {
+        NavigationManager.NavigateTo("Identity/Account/Logout", true);
+    }
+}
