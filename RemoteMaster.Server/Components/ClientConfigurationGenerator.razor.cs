a9f59529086b72e40372b242d609efef1313533a
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-10-09 10:13:47 +0500

    Реализовал возможность скачивания агента во время генерации конфигурации

--- RemoteMaster.Server/Components/ClientConfigurationGenerator.razor.cs
+++ RemoteMaster.Server/Components/ClientConfigurationGenerator.razor.cs
@@ -2,9 +2,19 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using Microsoft.AspNetCore.Components;
+
 namespace RemoteMaster.Server.Components;
 
 public partial class ClientConfigurationGenerator
 {
     private string _group;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    public void DownloadAgent()
+    {
+        NavigationManager.NavigateTo("api/ClientConfiguration/download-agent", true);
+    }
 }
