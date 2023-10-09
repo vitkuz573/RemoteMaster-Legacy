c432ca71634a5aa2aeff84e45fa918af20ef4c9f
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-10-09 12:31:40 +0500

    Реализовал подпись проектов агента, апдейтера и сервера при публикации

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -1,6 +1,9 @@
+param (
+    [string]$exeFilePath
+)
+
 $certSubject = "CN=RemoteMaster Development"
 $certStoreLocation = "Cert:\CurrentUser\My"
 $cert = Get-ChildItem $certStoreLocation | Where-Object { $_.Subject -eq $certSubject }
 
-Set-AuthenticodeSignature "\\SERVER-DC02\Win\RemoteMaster\Client\RemoteMaster.Client.exe" -Certificate $cert
-Get-AuthenticodeSignature -FilePath "\\SERVER-DC02\Win\RemoteMaster\Client\RemoteMaster.Client.exe"
+Set-AuthenticodeSignature $exeFilePath -Certificate $cert
