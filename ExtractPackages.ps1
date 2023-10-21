1533346ee577f4f66a7acdcd97c1324ba4fc7610
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-10-21 11:25:59 +0500

    Добавил скрипт для удаления неиспользуемых пакетов из Directory.Packages.props и очистил с его помощью Directory.Packages.props

---
+++ ExtractPackages.ps1
@@ -0,0 +1,71 @@
+# Directory containing the script and csproj files
+$solutionDir = "C:\Users\vitaly\source\repos\RemoteMaster"
+
+# Hashtable to store unique packages
+$packages = @{}
+
+# Function to extract packages from a csproj file
+function Extract-Packages {
+    param (
+        [string]$csprojPath
+    )
+
+    [xml]$csproj = Get-Content -Path $csprojPath
+    $packageReferences = $csproj.Project.ItemGroup.PackageReference
+
+    Write-Host "Analyzing $($csprojPath):" -ForegroundColor Magenta
+
+    foreach ($packageRef in $packageReferences) {
+        if ($null -ne $packageRef.Include) {
+            $packages[$packageRef.Include] = $true
+            Write-Host "  Found package: $($packageRef.Include)" -ForegroundColor DarkCyan
+        } else {
+            Write-Host "  Warning: Package reference without an 'Include' attribute found." -ForegroundColor Yellow
+        }
+    }
+}
+
+# Search for all csproj files in the directory
+$csprojFiles = Get-ChildItem -Path $solutionDir -Recurse -Filter "*.csproj"
+
+foreach ($csprojFile in $csprojFiles) {
+    Extract-Packages -csprojPath $csprojFile.FullName
+}
+
+# Load the Directory.Packages.props file as XML
+$packagePropsPath = "$solutionDir\Directory.Packages.props"
+[xml]$packagePropsXml = Get-Content -Path $packagePropsPath
+
+# Extract all PackageVersion elements
+$packageVersions = $packagePropsXml.Project.ItemGroup.PackageVersion
+
+# List to hold packages to remove
+$packagesToRemove = @()
+
+foreach ($packageVersion in $packageVersions) {
+    # Check if the package is not in the hashtable of unique packages
+    if (-not $packages.ContainsKey($packageVersion.Include)) {
+        $packagesToRemove += $packageVersion
+    }
+}
+
+if ($packagesToRemove.Count -gt 0) {
+    Write-Host "The following unused packages are identified:" -ForegroundColor Yellow
+    $packagesToRemove | ForEach-Object { Write-Host $_.Include -ForegroundColor Red }
+
+    # Ask for confirmation
+    $confirmation = Read-Host "Do you want to remove these packages? [y/N]"
+    if ($confirmation -eq 'y') {
+        # Remove unused packages from the XML
+        $packagesToRemove | ForEach-Object { $packagePropsXml.Project.ItemGroup.RemoveChild($_) }
+
+        # Save the modified XML back to the file
+        $packagePropsXml.OuterXml | Set-Content -Path $packagePropsPath
+
+        Write-Host "Removed the specified packages. Directory.Packages.props has been updated." -ForegroundColor Green
+    } else {
+        Write-Host "No changes made to Directory.Packages.props." -ForegroundColor Yellow
+    }
+} else {
+    Write-Host "No unused packages found. Directory.Packages.props remains unchanged." -ForegroundColor Yellow
+}
