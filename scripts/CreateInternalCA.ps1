539188e4172510e2cab0ab512f0cc41afcffbf30
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-10-29 09:53:54 +0500

    Переписал скрипт выдачи сертификата для ЦС (CA:TRUE)

--- scripts/CreateInternalCA.ps1
+++ scripts/CreateInternalCA.ps1
@@ -1,5 +1,6 @@
 $caName = "RemoteMaster Internal CA"
 $destDirectory = "InternalCA"
+$opensslPath = "C:\Program Files\OpenSSL-Win64\bin\openssl.exe"
 
 # Ensure destination directory exists
 if (-not (Test-Path $destDirectory)) {
@@ -7,36 +8,33 @@ if (-not (Test-Path $destDirectory)) {
     Write-Host "Created directory: $destDirectory" -ForegroundColor Green
 }
 
-# Certificate parameters
-$param = @{
-    DnsName           = $caName
-    KeyLength         = 4096
-    KeyAlgorithm      = "RSA"
-    HashAlgorithm     = "SHA256"
-    CertStoreLocation = "Cert:\CurrentUser\My"
-    KeyExportPolicy   = "Exportable"
-    NotAfter          = (Get-Date).AddYears(10) # Validity of 10 years
-    KeyUsage          = "CertSign"
-    FriendlyName      = $caName
-}
+# Generate a private key
+& $opensslPath genpkey -algorithm RSA -out "$destDirectory\$caName.key" -pkeyopt rsa_keygen_bits:4096
 
-# Prompt for password
-$password = Read-Host -Prompt "Enter a secure password for the CA private key" -AsSecureString
+# Generate a CSR using the private key
+& $opensslPath req -new -key "$destDirectory\$caName.key" -out "$destDirectory\$caName.csr" -subj "/CN=$caName"
 
-# Create the self-signed root CA certificate
-$caCert = New-SelfSignedCertificate @param
-Write-Host "Generated self-signed root CA certificate." -ForegroundColor Green
+# Create an OpenSSL configuration file
+$config = @"
+[ req ]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
+prompt = no
+[ req_distinguished_name ]
+CN = $caName
+[ v3_req ]
+basicConstraints = CA:TRUE
+"@
+$config | Out-File "$destDirectory\openssl.cnf" -Encoding ascii
 
-# Export root CA certificate to a PFX file with password
-$pfxPath = "$destDirectory\$caName.pfx"
-$caCert | Export-PfxCertificate -Password $password -FilePath $pfxPath
-Write-Host "Exported PFX to: $pfxPath" -ForegroundColor Green
+# Generate a self-signed certificate with Basic Constraints set to CA:TRUE
+& $opensslPath x509 -req -days 3650 -in "$destDirectory\$caName.csr" -signkey "$destDirectory\$caName.key" -out "$destDirectory\$caName.crt" -extfile "$destDirectory\openssl.cnf" -extensions v3_req
 
-# Export root CA certificate to a CER file (public key only)
-$cerPath = "$destDirectory\$caName.cer"
-$caCert | Export-Certificate -FilePath $cerPath
-Write-Host "Exported CER to: $cerPath" -ForegroundColor Green
+# Convert the certificate and private key to a PFX file
+& $opensslPath pkcs12 -export -out "$destDirectory\$caName.pfx" -inkey "$destDirectory\$caName.key" -in "$destDirectory\$caName.crt" -name $caName
 
-# Add root CA certificate to Trusted Root Certification Authorities store
-Import-Certificate -FilePath $cerPath -CertStoreLocation "Cert:\CurrentUser\Root"
+Write-Host "Generated self-signed root CA certificate and exported to PFX." -ForegroundColor Green
+
+# Import the certificate to the Trusted Root Certification Authorities store
+Import-Certificate -FilePath "$destDirectory\$caName.crt" -CertStoreLocation "Cert:\CurrentUser\Root"
 Write-Host "Added CA certificate to Trusted Root Certification Authorities store." -ForegroundColor Green
