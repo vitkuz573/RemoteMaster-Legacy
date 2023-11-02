fb6af8a1a000950b441b3bee9759f7fd51d020b9
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-11-02 10:35:13 +0500

    Вынес ввод в Connect.Input.cs

--- RemoteMaster.Server/Pages/Connect.razor.cs
+++ RemoteMaster.Server/Pages/Connect.razor.cs
@@ -13,7 +13,6 @@ using Microsoft.JSInterop;
 using Polly;
 using Polly.Retry;
 using RemoteMaster.Server.Models;
-using RemoteMaster.Shared.Dtos;
 using RemoteMaster.Shared.Models;
 
 namespace RemoteMaster.Server.Pages;
@@ -228,70 +227,6 @@ public partial class Connect : IDisposable
         var percentY = (e.ClientY - imgPosition.Top) / imgPosition.Height;
 
         return new PointD(percentX, percentY);
-    }
-
-    private async Task OnMouseMove(MouseEventArgs e)
-    {
-        var xyPercent = await GetRelativeMousePositionPercentAsync(e);
-
-        await SafeInvokeAsync(() => _connection.InvokeAsync("SendMouseCoordinates", new MouseMoveDto
-        {
-            X = xyPercent.X,
-            Y = xyPercent.Y
-        }));
-    }
-
-    private async Task OnMouseUpDown(MouseEventArgs e)
-    {
-        var state = e.Type == "mouseup" ? ButtonState.Up : ButtonState.Down;
-        await SendMouseInputAsync(e, state);
-    }
-
-    private async Task OnMouseOver(MouseEventArgs e)
-    {
-        await SendMouseInputAsync(e, ButtonState.Up);
-    }
-
-    private async Task SendMouseInputAsync(MouseEventArgs e, ButtonState state)
-    {
-        var xyPercent = await GetRelativeMousePositionPercentAsync(e);
-
-        await SafeInvokeAsync(() => _connection.InvokeAsync("SendMouseButton", new MouseClickDto
-        {
-            Button = e.Button,
-            State = state,
-            X = xyPercent.X,
-            Y = xyPercent.Y
-        }));
-    }
-
-    private async Task OnMouseWheel(WheelEventArgs e)
-    {
-        await SafeInvokeAsync(() => _connection.InvokeAsync("SendMouseWheel", new MouseWheelDto
-        {
-            DeltaY = (int)e.DeltaY
-        }));
-    }
-
-    private async Task SendKeyboardInput(int keyCode, ButtonState state)
-    {
-        await SafeInvokeAsync(() => _connection.InvokeAsync("SendKeyboardInput", new KeyboardKeyDto
-        {
-            Key = keyCode,
-            State = state
-        }));
-    }
-
-    [JSInvokable]
-    public async Task OnKeyDown(int keyCode)
-    {
-        await SendKeyboardInput(keyCode, ButtonState.Down);
-    }
-
-    [JSInvokable]
-    public async Task OnKeyUp(int keyCode)
-    {
-        await SendKeyboardInput(keyCode, ButtonState.Up);
     }
 
     private async Task ToggleMenu()
