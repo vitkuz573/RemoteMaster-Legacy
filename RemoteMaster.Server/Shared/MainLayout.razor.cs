10644be8c7329375772c37b7ed96ba8827ceb4f3
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-11-04 22:11:28 +0500

    UPD

---
+++ RemoteMaster.Server/Shared/MainLayout.razor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using MudBlazor;
+
+namespace RemoteMaster.Server.Shared;
+
+public partial class MainLayout
+{
+    private bool _isDarkMode = false;
+
+    private readonly MudTheme _theme = new()
+    {
+        LayoutProperties = new LayoutProperties()
+        {
+            DrawerWidthRight = "300px"
+        }
+    };
+}
