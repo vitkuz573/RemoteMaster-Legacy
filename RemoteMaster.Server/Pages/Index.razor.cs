59c12da50aeb81d7317afc2aed678ce9b30be8bb
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-11-05 17:48:42 +0500

    UPD

--- RemoteMaster.Server/Pages/Index.razor.cs
+++ RemoteMaster.Server/Pages/Index.razor.cs
@@ -2,7 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-using System.Diagnostics;
 using System.Net.NetworkInformation;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
