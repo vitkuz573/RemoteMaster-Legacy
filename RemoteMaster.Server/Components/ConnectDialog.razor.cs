ee5d28e02540706d7c4e2ddbb53a54a7da161d0f
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-11-06 09:51:53 +0500

    Добавил общий диалог

--- RemoteMaster.Server/Components/ConnectDialog.razor.cs
+++ RemoteMaster.Server/Components/ConnectDialog.razor.cs
@@ -3,38 +3,23 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.JSInterop;
 using MudBlazor;
 using RemoteMaster.Server.Abstractions;
-using RemoteMaster.Server.Models;
 
 namespace RemoteMaster.Server.Components;
 
-#pragma warning disable CA2227
-
-public partial class ConnectDialog
+public partial class ConnectDialog : CommonDialogBase
 {
-    [CascadingParameter]
-    private MudDialogInstance MudDialog { get; set; }
-
-    [Parameter]
-    public Dictionary<Computer, HubConnection> Hosts { get; set; }
-
     [Inject]
     private IComputerCommandService ComputerCommandService { get; set; }
 
     [Inject]
     private IJSRuntime JSRuntime { get; set; }
 
-    private string _selectedOption;
+    protected string _selectedOption;
 
-    private void Cancel()
-    {
-        MudDialog.Cancel();
-    }
-
-    private async Task Connect()
+    protected async Task Connect()
     {
         if (_selectedOption == "control")
         {
@@ -48,7 +33,7 @@ public partial class ConnectDialog
         MudDialog.Close(DialogResult.Ok(true));
     }
 
-    private async Task OpenWindow(string url)
+    protected async Task OpenWindow(string url)
     {
         await JSRuntime.InvokeVoidAsync("openNewWindow", url);
     }
