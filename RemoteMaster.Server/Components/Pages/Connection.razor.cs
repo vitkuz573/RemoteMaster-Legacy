af6c0eb45de44d25b0937812c3ebd06c1d0e260c
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-11-28 22:39:21 +0500

    UPD

--- RemoteMaster.Server/Components/Pages/Connection.razor.cs
+++ RemoteMaster.Server/Components/Pages/Connection.razor.cs
@@ -3,6 +3,13 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using Microsoft.AspNetCore.Components;
+using Polly.Retry;
+using Polly;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
+using RemoteMaster.Server.Models;
 
 namespace RemoteMaster.Server.Components.Pages;
 
@@ -11,10 +18,76 @@ public partial class Connection
     [Parameter]
     public string Host { get; set; } = default!;
 
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; }
+
     private bool _drawerOpen = false;
+
+    private HubConnection _connection;
+
+    private readonly AsyncRetryPolicy _retryPolicy = Policy
+        .Handle<Exception>()
+        .WaitAndRetryAsync(new[]
+        {
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(7),
+                TimeSpan.FromSeconds(10),
+        });
 
     private void DrawerToggle()
     {
         _drawerOpen = !_drawerOpen;
+    }
+
+    private async Task SafeInvokeAsync(Func<Task> action)
+    {
+        await _retryPolicy.ExecuteAsync(async () =>
+        {
+            if (_connection.State == HubConnectionState.Connected)
+            {
+                try
+                {
+                    await action();
+                }
+                catch (HubException ex) when (ex.Message.Contains("Method does not exist"))
+                {
+                    await JSRuntime.InvokeVoidAsync("showAlert", "This function is not available in the current host version. Please update your host.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Connection is not active");
+            }
+        });
+    }
+
+    private async Task<PointD> GetRelativeMousePositionPercentAsync(MouseEventArgs e)
+    {
+        var imgElement = await JSRuntime.InvokeAsync<IJSObjectReference>("document.getElementById", "screenImage");
+        var imgPosition = await imgElement.InvokeAsync<DOMRect>("getBoundingClientRect");
+        var percentX = (e.ClientX - imgPosition.Left) / imgPosition.Width;
+        var percentY = (e.ClientY - imgPosition.Top) / imgPosition.Height;
+
+        return new PointD(percentX, percentY);
+    }
+
+    private async Task KillHost()
+    {
+        await SafeInvokeAsync(() => _connection.InvokeAsync("SendKillHost"));
+    }
+
+    private async Task SendCtrlAltDel()
+    {
+        await SafeInvokeAsync(() => _connection.InvokeAsync("SendCommandToService", "CtrlAltDel"));
+    }
+
+    private async Task RebootComputer()
+    {
+        await SafeInvokeAsync(() => _connection.InvokeAsync("SendRebootComputer", string.Empty, 0, true));
+    }
+
+    private async Task ShutdownComputer()
+    {
+        await SafeInvokeAsync(() => _connection.InvokeAsync("SendShutdownComputer", string.Empty, 0, true));
     }
 }
