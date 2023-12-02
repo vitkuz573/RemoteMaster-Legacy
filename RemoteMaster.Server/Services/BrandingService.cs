82bdcf512518417b0835edd3f7f42a5182230445
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-02 14:09:21 +0500

    UPD

--- RemoteMaster.Server/Services/BrandingService.cs
+++ RemoteMaster.Server/Services/BrandingService.cs
@@ -2,11 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-
-// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
-// This file is part of the RemoteMaster project.
-// Licensed under the GNU Affero General Public License v3.0.
-
 using RemoteMaster.Server.Abstractions;
 
 namespace RemoteMaster.Server.Services;
