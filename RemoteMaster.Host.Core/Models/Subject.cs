59f94d212d68bcc602a8d9e07aa164ed4f648db0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-04 06:35:44 +0500

    Добавил модель с информацией о субьекте

---
+++ RemoteMaster.Host.Core/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Text.Json.Serialization;
+
+namespace RemoteMaster.Host.Core.Models;
+
+public class Subject
+{
+    [JsonPropertyName("organization")]
+    public string Organization { get; set; }
+
+    [JsonPropertyName("locality")]
+    public string Locality { get; set; }
+
+    [JsonPropertyName("state")]
+    public string State { get; set; }
+
+    [JsonPropertyName("Country")]
+    public string Country { get; set; }
+}
