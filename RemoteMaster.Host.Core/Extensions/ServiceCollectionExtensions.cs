ac6626ae01060800cea96ddf6f3151eed1bd91d4
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-04 06:58:14 +0500

    UPD

--- RemoteMaster.Host.Core/Extensions/ServiceCollectionExtensions.cs
+++ RemoteMaster.Host.Core/Extensions/ServiceCollectionExtensions.cs
@@ -2,17 +2,21 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RemoteMaster.Host.Core.Abstractions;
+using RemoteMaster.Host.Core.Models;
 using RemoteMaster.Host.Core.Services;
 
 namespace RemoteMaster.Host.Core.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         services.AddLogging(builder =>
         {
             builder.ClearProviders();
@@ -26,6 +30,8 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IAppState, AppState>();
         services.AddSingleton<IShutdownService, ShutdownService>();
         services.AddTransient<IViewerFactory, ViewerFactory>();
+        services.Configure<Subject>(configuration.GetSection("subject"));
+
 
         services.AddSignalR().AddMessagePackProtocol();
 
