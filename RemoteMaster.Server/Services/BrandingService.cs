092898353ed5cbbc6d8f18cf05e12223f21e5a2d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-07 16:28:36 +0500

    UPD

--- RemoteMaster.Server/Services/BrandingService.cs
+++ RemoteMaster.Server/Services/BrandingService.cs
@@ -8,11 +8,11 @@ namespace RemoteMaster.Server.Services;
 
 public class BrandingService : IBrandingService
 {
-    public string ApplicationName => "RemoteMaster Server";
+    public string ApplicationName => "RemoteMaster хуй";
 
-    public string ApplicationLogo => "/img/logo.jpg";
+    public string ApplicationLogo => "/img/logo.png";
 
-    public string ApplicationLogoWidth => "100";
+    public string ApplicationLogoWidth => "300";
 
     public string ApplicationLogoHeight => "auto";
 }
