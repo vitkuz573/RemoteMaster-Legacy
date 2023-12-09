1e3f3807eb64f055797ce3c3e19f57c1e1328357
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-09 16:18:54 +0500

    UPD

--- RemoteMaster.Shared/Dtos/MouseClickDto.cs
+++ RemoteMaster.Shared/Dtos/MouseClickDto.cs
@@ -2,8 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-using RemoteMaster.Shared.Models;
-
 namespace RemoteMaster.Shared.Dtos;
 
 public class MouseClickDto
