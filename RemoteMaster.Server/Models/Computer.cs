f711e24afef650a970aa4f10d8db2b39e13d978d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-11 09:47:47 +0500

    Удалил ненужный код из класса Computer

--- RemoteMaster.Server/Models/Computer.cs
+++ RemoteMaster.Server/Models/Computer.cs
@@ -9,19 +9,9 @@ namespace RemoteMaster.Server.Models;
 
 public class Computer : Node
 {
-    public Computer()
-    {
-    }
+    public required string IPAddress { get; set; }
 
-    public Computer(string name, string ipAddress)
-    {
-        Name = name;
-        IPAddress = ipAddress;
-    }
-
-    public string IPAddress { get; set; }
-
-    public string MACAddress { get; set; }
+    public required string MACAddress { get; set; }
 
     [NotMapped]
     public byte[]? Thumbnail { get; set; }
