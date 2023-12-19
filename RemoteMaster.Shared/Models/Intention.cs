5efaba90bf50da9cea50621fbf3e0445cc393635
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-19 10:26:45 +0500

    Добавил комментарии в `Intention`

--- RemoteMaster.Shared/Models/Intention.cs
+++ RemoteMaster.Shared/Models/Intention.cs
@@ -6,6 +6,13 @@ namespace RemoteMaster.Shared.Models;
 
 public enum Intention
 {
+    /// <summary>
+    /// Used to receive a thumbnail image from the remote device.
+    /// </summary>
     ReceiveThumbnail,
+
+    /// <summary>
+    /// Used for general management and control operations on the remote device.
+    /// </summary>
     ManageDevice
 }
