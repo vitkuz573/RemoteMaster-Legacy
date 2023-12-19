e0b4265784ef25e28bc16b70aa2222095b6a614a
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-19 12:08:06 +0500

    UPD

--- RemoteMaster.Server/Components/Dialogs/PsexecRulesDialog.razor.cs
+++ RemoteMaster.Server/Components/Dialogs/PsexecRulesDialog.razor.cs
@@ -2,35 +2,20 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.JSInterop;
 using MudBlazor;
 using RemoteMaster.Server.Abstractions;
-using RemoteMaster.Shared.Models;
 
 namespace RemoteMaster.Server.Components.Dialogs;
 
-#pragma warning disable CA2227
-
 public partial class PsexecRulesDialog
 {
-    [CascadingParameter]
-    private MudDialogInstance MudDialog { get; set; }
-
-    [Parameter]
-    public ConcurrentDictionary<Computer, HubConnection?> Hosts { get; set; }
-
     [Inject]
     private IComputerCommandService ComputerCommandService { get; set; }
 
     private bool _selectedOption;
-
-    private void Cancel()
-    {
-        MudDialog.Cancel();
-    }
 
     private async Task Ok()
     {
