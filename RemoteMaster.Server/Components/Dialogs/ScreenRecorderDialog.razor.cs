b9b79e5ae90bcb1e7b34edcf40688ae5bffad515
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-19 12:00:59 +0500

    UPD

--- RemoteMaster.Server/Components/Dialogs/ScreenRecorderDialog.razor.cs
+++ RemoteMaster.Server/Components/Dialogs/ScreenRecorderDialog.razor.cs
@@ -2,27 +2,17 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
 using MudBlazor;
 using RemoteMaster.Server.Abstractions;
-using RemoteMaster.Shared.Models;
 
 namespace RemoteMaster.Server.Components.Dialogs;
 
-#pragma warning disable CA2227
-
 public partial class ScreenRecorderDialog
 {
-    [CascadingParameter]
-    private MudDialogInstance MudDialog { get; set; }
-
-    [Parameter]
-    public ConcurrentDictionary<Computer, HubConnection?> Hosts { get; set; }
-
     [Inject]
-    private IComputerCommandService ComputerCommandService { get; set; }
+    private IComputerCommandService ComputerCommandService { get; set; } = default!;
 
     private string _outputFileName;
     private uint _duration;
@@ -34,11 +24,6 @@ public partial class ScreenRecorderDialog
         _outputFileName = $@"C:\{requesterName}_{currentDate}.mp4";
 
         base.OnInitialized();
-    }
-
-    private void Cancel()
-    {
-        MudDialog.Cancel();
     }
 
     private async Task StartRecording()
