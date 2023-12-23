ed2793af3185cb2d46286c450a933bd5bf92d478
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-12-23 21:27:09 +0500

    Улучшил представление диспетчера задач

--- RemoteMaster.Server/Components/Pages/TaskManager.razor.cs
+++ RemoteMaster.Server/Components/Pages/TaskManager.razor.cs
@@ -110,6 +110,21 @@ public partial class TaskManager : IDisposable
         await InvokeAsync(StateHasChanged);
     }
 
+    private string FormatSize(long size)
+    {
+        string[] sizes = ["B", "KB", "MB", "GB", "TB"];
+        double len = size;
+        var order = 0;
+
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len /= 1024;
+        }
+
+        return $"{len:0.##} {sizes[order]}";
+    }
+
     [JSInvokable]
     public void OnBeforeUnload()
     {
