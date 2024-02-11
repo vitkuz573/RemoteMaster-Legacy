e4536f23185caf7684e98d17fe5779430a72473b
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-02-11 15:13:22 +0500

    UPD

---
+++ RemoteMaster.Host.Windows/Services/PsExecService.cs
@@ -0,0 +1,68 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Serilog;
+using System.Diagnostics;
+using System.Globalization;
+using RemoteMaster.Host.Core.Abstractions;
+
+namespace RemoteMaster.Host.Windows.Services;
+
+public class PsExecService : IPsExecService
+{
+    private readonly Dictionary<string, string> _ruleGroupNames = new()
+    {
+        { "en-US", "Remote Service Management" },
+        { "ru-RU", "Удаленное управление службой" }
+    };
+
+    public void Enable()
+    {
+        ExecuteCommand("winrm qc -force");
+        ExecuteCommand(@"netsh AdvFirewall firewall add rule name=PSExec dir=In action=allow protocol=TCP localport=RPC profile=domain,private program=""%WinDir%\system32\services.exe"" service=any");
+        var localizedRuleGroupName = GetLocalizedRuleGroupName();
+        ExecuteCommand($@"netsh AdvFirewall firewall set rule group=""{localizedRuleGroupName}"" new enable=yes");
+
+        Log.Information("PsExec and WinRM configurations have been enabled.");
+    }
+
+    public void Disable()
+    {
+        ExecuteCommand("netsh AdvFirewall firewall delete rule name=PSExec");
+        Log.Information("PsExec rule has been disabled.");
+    }
+
+    private static void ExecuteCommand(string command)
+    {
+        try
+        {
+            var processStartInfo = new ProcessStartInfo("cmd.exe", $"/c {command}")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            var result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Log.Information($"Executed command: {command}\nResult: {result}");
+        }
+        catch (Exception ex)
+        {
+            Log.Error($"Error executing command: {command}. Exception: {ex.Message}");
+        }
+    }
+
+    private string GetLocalizedRuleGroupName()
+    {
+        var currentCulture = CultureInfo.CurrentCulture.Name;
+        
+        return _ruleGroupNames.TryGetValue(currentCulture, out var localizedGroupName) ? localizedGroupName : _ruleGroupNames["en-US"];
+    }
+}
