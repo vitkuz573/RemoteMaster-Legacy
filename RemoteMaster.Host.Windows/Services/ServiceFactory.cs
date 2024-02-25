2f7e23b75b62b5473c49527a058593eb9352f491
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-02-25 13:21:38 +0500

    UPD

--- RemoteMaster.Host.Windows/Services/ServiceFactory.cs
+++ RemoteMaster.Host.Windows/Services/ServiceFactory.cs
@@ -2,20 +2,41 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using System.Reflection;
 using RemoteMaster.Host.Windows.Abstractions;
-using RemoteMaster.Host.Windows.Models;
 
 namespace RemoteMaster.Host.Windows.Services;
 
 public class ServiceFactory : IServiceFactory
 {
+    private readonly Dictionary<string, Func<AbstractService>> _serviceCreators = [];
+
+    public ServiceFactory()
+    {
+        LoadAllServices();
+    }
+
+    private void LoadAllServices()
+    {
+        var serviceTypes = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t is { IsClass: true, IsAbstract: false } && typeof(AbstractService).IsAssignableFrom(t));
+
+        foreach (var type in serviceTypes)
+        {
+            if (Activator.CreateInstance(type) is AbstractService serviceInstance)
+            {
+                _serviceCreators[serviceInstance.Name] = () => (AbstractService)Activator.CreateInstance(type);
+            }
+        }
+    }
+
     public AbstractService GetService(string serviceName)
     {
-        return serviceName switch
+        if (_serviceCreators.TryGetValue(serviceName, out var constructor))
         {
-            "RCHost" => new HostService(),
-            "RCUpdater" => new UpdaterService(),
-            _ => throw new ArgumentException($"Service for '{serviceName}' is not defined.")
-        };
+            return constructor();
+        }
+
+        throw new ArgumentException($"Service for '{serviceName}' is not defined.");
     }
 }
