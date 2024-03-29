0bed84bb66e6b02bd9693294f0c3dbefd0675303
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-03-29 12:45:17 +0500

    UPD

--- RemoteMaster.Host.Core/Services/HostRegistrationMonitorService.cs
+++ RemoteMaster.Host.Core/Services/HostRegistrationMonitorService.cs
@@ -4,7 +4,6 @@
 
 using Microsoft.Extensions.Hosting;
 using RemoteMaster.Host.Core.Abstractions;
-using RemoteMaster.Shared.Models;
 using Serilog;
 
 namespace RemoteMaster.Host.Core.Services;
@@ -23,57 +22,39 @@ public class HostRegistrationMonitorService : IHostedService
         _hostConfigurationService = hostConfigurationService;
         _hostInformationMonitorService = hostInformationMonitorService;
 
-        _timer = new Timer(CheckHostRegistration, null, Timeout.Infinite, Timeout.Infinite);
+        _timer = new Timer(CheckHostRegistration, null, Timeout.Infinite, 0);
     }
 
-    public async Task StartAsync(CancellationToken cancellationToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        var configurationChanged = await _hostInformationMonitorService.UpdateHostConfigurationAsync();
-
-        var hostConfiguration = await _hostConfigurationService.LoadConfigurationAsync(false);
-
-        if (configurationChanged)
-        {
-            await HandleHostRegistration(hostConfiguration);
-        }
-
-        if (hostConfiguration != null)
-        {
-            if (_hostInformationMonitorService.CheckCertificateExpiration())
-            {
-                await _hostLifecycleService.RenewCertificateAsync(hostConfiguration);
-            }
-        }
-
         _timer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+        return Task.CompletedTask;
     }
 
     private async void CheckHostRegistration(object? state)
     {
         try
         {
+            var configurationChanged = await _hostInformationMonitorService.UpdateHostConfigurationAsync();
             var hostConfiguration = await _hostConfigurationService.LoadConfigurationAsync(false);
-            await HandleHostRegistration(hostConfiguration);
+            var isHostRegistered = await _hostLifecycleService.IsHostRegisteredAsync(hostConfiguration);
+
+            switch (isHostRegistered)
+            {
+                case true when configurationChanged:
+                    await _hostLifecycleService.UpdateHostInformationAsync(hostConfiguration);
+                    Log.Information("Host information updated due to configuration change.");
+                    break;
+                case false:
+                    Log.Warning("Host is not registered. Performing necessary actions...");
+                    await _hostLifecycleService.RegisterAsync(hostConfiguration);
+                    break;
+            }
         }
         catch (Exception ex)
         {
             Log.Error(ex, "Error during host registration check.");
-        }
-    }
-
-    private async Task HandleHostRegistration(HostConfiguration hostConfiguration)
-    {
-        var isHostRegistered = await _hostLifecycleService.IsHostRegisteredAsync(hostConfiguration);
-
-        if (!isHostRegistered)
-        {
-            Log.Warning("Host is not registered. Performing necessary actions...");
-            await _hostLifecycleService.RegisterAsync(hostConfiguration);
-        }
-        else
-        {
-            Log.Information("Updating host information due to configuration change.");
-            await _hostLifecycleService.UpdateHostInformationAsync(hostConfiguration);
         }
     }
 
