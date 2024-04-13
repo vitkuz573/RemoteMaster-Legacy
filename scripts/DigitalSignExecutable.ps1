6a99308802c0b2d0de1a51ae66d1a2d8c8b77ebd
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-04-13 14:16:59 +0500

    UPD

--- scripts/DigitalSignExecutable.ps1
+++ scripts/DigitalSignExecutable.ps1
@@ -1,10 +1,53 @@
 param (
+    [Parameter(Mandatory = $true)]
+    [ValidateScript({Test-Path $_ -PathType Leaf})]
     [string]$exeFilePath
 )
 
 $certSubject = "CN=RemoteMaster Development"
 $certStoreLocation = "Cert:\CurrentUser\My"
-$cert = Get-ChildItem $certStoreLocation | Where-Object { $_.Subject -eq $certSubject }
 
-Set-AuthenticodeSignature $exeFilePath -Certificate $cert
+function Write-OutputMessage {
+    Param([string]$message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "${timestamp}: $message"
+}
 
+function Load-Certificate {
+    Write-OutputMessage "Searching for certificate with subject '$certSubject'."
+    $certs = Get-ChildItem $certStoreLocation | Where-Object { $_.Subject -eq $certSubject } | Sort-Object NotBefore -Descending
+    $cert = $certs | Where-Object { $_.NotAfter -gt (Get-Date) } | Select-Object -First 1
+
+    if (-not $cert) {
+        Write-OutputMessage "Error: Valid certificate not found. Check if the certificate has expired or does not exist."
+        throw "No valid certificate found."
+    }
+    Write-OutputMessage "Certificate with Subject '$certSubject' found. Valid until $($cert.NotAfter)."
+    return $cert
+}
+
+Write-OutputMessage "Script started for signing '$exeFilePath'."
+
+try {
+    $cert = Load-Certificate
+} catch {
+    Write-OutputMessage "Error during certificate loading: $_"
+    exit 1
+}
+
+try {
+    $signingResult = Set-AuthenticodeSignature -FilePath $exeFilePath -Certificate $cert
+    Write-OutputMessage "Signing Status: $($signingResult.Status)"
+    
+    if ($signingResult.Status -ne 'Valid') {
+        Write-OutputMessage "Failed to apply a valid digital signature. Error: $($signingResult.Status)"
+        exit 1
+    } else {
+        Write-OutputMessage "Successfully signed the executable: $exeFilePath"
+    }
+} catch {
+    Write-OutputMessage "Exception occurred during signing: $_"
+    exit 1
+}
+
+Write-OutputMessage "Script completed successfully."
