25e2ed6e52b1b0a9f25387cca73792a9f7005b67
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-01 14:23:06 +0500

    UPD

--- RemoteMaster.Server/Middlewares/RouteRestrictionMiddleware.cs
+++ RemoteMaster.Server/Middlewares/RouteRestrictionMiddleware.cs
@@ -2,44 +2,59 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using Microsoft.AspNetCore.Identity;
+using RemoteMaster.Server.Data;
+
 namespace RemoteMaster.Server.Middlewares;
 
 public class RouteRestrictionMiddleware(RequestDelegate next)
 {
+    private static readonly List<string> RootAdminExclusiveRoutes =
+    [
+        "/admin"
+    ];
+
     private static readonly List<string> RestrictedRoutes =
     [
         "/account/manage/personaldata",
         "/account/manage/deletepersonaldata"
     ];
 
-    private static readonly List<string> AllowedRoutes =
-    [
-        "/account/login",
-        "/account/logout",
-        "/account/manage",
-        "/account/register"
-    ];
-
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context, UserManager<ApplicationUser> userManager)
     {
         ArgumentNullException.ThrowIfNull(context);
+        ArgumentNullException.ThrowIfNull(userManager);
 
         var path = context.Request.Path.Value?.ToLower() ?? string.Empty;
+        var user = await userManager.GetUserAsync(context.User);
 
-        if (RestrictedRoutes.Contains(path))
+        if (user != null)
         {
-            context.Response.StatusCode = 404;
+            var userRoles = await userManager.GetRolesAsync(user);
+            
+            var isRootAdmin = userRoles.Contains("RootAdministrator");
+            var isAdmin = userRoles.Contains("Administrator");
 
-            return;
-        }
+            if (isRootAdmin && RootAdminExclusiveRoutes.Any(path.StartsWith))
+            {
+                await next(context);
 
-        var isAllowedRoute = AllowedRoutes.Any(path.StartsWith);
+                return;
+            }
 
-        if (path.StartsWith("/account") && !isAllowedRoute)
-        {
-            context.Response.Redirect("/");
+            if (!isRootAdmin && (RestrictedRoutes.Contains(path) || path.StartsWith("/admin")))
+            {
+                context.Response.StatusCode = 403;
 
-            return;
+                return;
+            }
+
+            if (path.StartsWith("/account") && (!isRootAdmin && !isAdmin))
+            {
+                context.Response.Redirect("/");
+
+                return;
+            }
         }
 
         await next(context);
