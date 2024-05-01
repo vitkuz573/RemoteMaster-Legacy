6556ba335b85f1804562ed0f08b8e63e2473a1e6
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-01 08:07:01 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Admin/Pages/ManageOrganizations.razor.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace RemoteMaster.Server.Components.Admin.Pages;
+
+[Authorize(Roles = "RootAdministrator")]
+public partial class ManageOrganizations
+{
+    [CascadingParameter]
+    private HttpContext HttpContext { get; set; } = default!;
+
+    [SupplyParameterFromForm]
+    private InputModel Input { get; set; } = new();
+
+    protected async override Task OnInitializedAsync()
+    {
+
+    }
+
+    private sealed class InputModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
