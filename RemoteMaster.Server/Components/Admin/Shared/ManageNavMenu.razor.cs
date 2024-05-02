48d352c8110231fd17981e757c83f74f8f6fca91
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-02 13:46:00 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Admin/Shared/ManageNavMenu.razor.cs
@@ -0,0 +1,36 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+
+using System.Security.Claims;
+
+namespace RemoteMaster.Server.Components.Admin.Shared;
+
+public partial class ManageNavMenu
+{
+    private string _username;
+    private string _role;
+
+    protected async override Task OnInitializedAsync()
+    {
+        var authenticationState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var userPrincipal = authenticationState.User;
+
+        if (userPrincipal.Identity.IsAuthenticated)
+        {
+            var userId = userPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId != null)
+            {
+                var user = await UserManager.FindByIdAsync(userId);
+
+                var roles = await UserManager.GetRolesAsync(user);
+
+                _role = roles.FirstOrDefault();
+            }
+        }
+
+        _username = userPrincipal.Identity.Name;
+    }
+}
