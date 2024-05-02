10ab47e2b35ca5c614f0561d2687caaea3a4aaf7
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-02 22:03:53 +0500

    UPD

--- RemoteMaster.Server/Components/Admin/Pages/Manage/ManageCertificateAuthority.razor.cs
+++ RemoteMaster.Server/Components/Admin/Pages/Manage/ManageCertificateAuthority.razor.cs
@@ -2,9 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-using Microsoft.AspNetCore.Components;
-using RemoteMaster.Server.Models;
-
 namespace RemoteMaster.Server.Components.Admin.Pages.Manage;
 
 public partial class ManageCertificateAuthority
@@ -12,9 +9,6 @@ public partial class ManageCertificateAuthority
     private string _commonName;
     private int _keySize;
     private int _validityPeriod;
-
-    [SupplyParameterFromForm]
-    private CertificateOptions CertificateDetails { get; set; } = new();
 
     protected override void OnInitialized()
     {
---
+++ RemoteMaster.Server/Components/Admin/Pages/Manage/ManageJwt.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using RemoteMaster.Server.Models;
+
+namespace RemoteMaster.Server.Components.Admin.Pages.Manage;
+
+public partial class ManageJwt
+{
+    private string _keysDirectory;
+    private int _keySize;
+
+    protected override void OnInitialized()
+    {
+        var jwt = Jwt.Value;
+
+        _keysDirectory = jwt.KeysDirectory;
+        _keySize = jwt.KeySize;
+    }
+}
