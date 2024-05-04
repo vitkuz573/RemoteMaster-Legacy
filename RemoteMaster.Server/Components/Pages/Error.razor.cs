29b82bd50487668967549226e3d8214083e74d90
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-04 07:00:37 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Pages/Error.razor.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+
+namespace RemoteMaster.Server.Components.Pages;
+
+public partial class Error
+{
+    [CascadingParameter]
+    private HttpContext? HttpContext { get; set; }
+
+    private string? RequestId { get; set; }
+
+    private bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    protected override void OnInitialized()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext?.TraceIdentifier;
+    }
+}
