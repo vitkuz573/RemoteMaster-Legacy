0f898bda5b06a22a2966a1db2fb66b99f16a1bbc
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-06 20:53:45 +0500

    New interface

---
+++ RemoteMaster.Server/Components/Pages/AccessNew.razor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+
+namespace RemoteMaster.Server.Components.Pages;
+
+public partial class AccessNew
+{
+    [Parameter]
+    public string Host { get; set; }
+
+    private bool IsSidebarOpen = false;
+
+    private void ToggleSidebar()
+    {
+        IsSidebarOpen = !IsSidebarOpen;
+    }
+}
