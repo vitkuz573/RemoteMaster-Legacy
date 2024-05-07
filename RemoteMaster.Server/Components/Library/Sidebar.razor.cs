21d429110cd989593ba765ced3c15a2a243eeafe
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-07 21:19:50 +0500

    UPD

--- RemoteMaster.Server/Components/Library/Sidebar.razor.cs
+++ RemoteMaster.Server/Components/Library/Sidebar.razor.cs
@@ -11,10 +11,42 @@ public partial class Sidebar
     [Parameter]
     public RenderFragment ChildContent { get; set; }
 
-    private bool _isSidebarOpen = false;
+    [Parameter]
+    public string Title { get; set; } = "Settings";
+
+    [Parameter]
+    public string IconOpen { get; set; } = "chevron_left";
+
+    [Parameter]
+    public string IconClosed { get; set; } = "chevron_right";
+
+    [Parameter]
+    public string CustomClass { get; set; } = "bg-gray-800 text-white";
+
+    [Parameter]
+    public int WidthPx { get; set; } = 256;
+
+    [Parameter]
+    public bool StartOpen { get; set; } = false;
+
+    [Parameter]
+    public bool IsDisabled { get; set; } = false;
+
+    [Parameter]
+    public int AnimationDurationMs { get; set; } = 500;
+
+    private bool _isSidebarOpen;
+
+    protected override void OnInitialized()
+    {
+        _isSidebarOpen = StartOpen;
+    }
 
     private void ToggleSidebar()
     {
-        _isSidebarOpen = !_isSidebarOpen;
+        if (!IsDisabled)
+        {
+            _isSidebarOpen = !_isSidebarOpen;
+        }
     }
 }
