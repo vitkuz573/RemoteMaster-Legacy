6fd967e47dd1aad9dd9f74dc32c143179e4042d0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-07 17:53:33 +0500

    UPD

--- RemoteMaster.Server/Components/Library/ExpandablePanel.razor.cs
+++ RemoteMaster.Server/Components/Library/ExpandablePanel.razor.cs
@@ -14,10 +14,39 @@ public partial class ExpandablePanel
     [Parameter]
     public RenderFragment ChildContent { get; set; }
 
-    private bool IsExpanded { get; set; } = false;
+    [Parameter]
+    public string Icon { get; set; } = "default-icon";
+
+    [Parameter]
+    public bool StartExpanded { get; set; } = false;
+
+    [Parameter]
+    public bool IsDisabled { get; set; } = false;
+
+    [Parameter]
+    public string CustomClass { get; set; }
+
+    [Parameter]
+    public string AriaLabel { get; set; }
+
+    [Parameter]
+    public int AnimationDurationMs { get; set; } = 300;
+
+    [Parameter]
+    public RenderFragment HeaderContent { get; set; }
+
+    private bool IsExpanded { get; set; }
+
+    protected override void OnInitialized()
+    {
+        IsExpanded = StartExpanded;
+    }
 
     private void Toggle()
     {
-        IsExpanded = !IsExpanded;
+        if (!IsDisabled)
+        {
+            IsExpanded = !IsExpanded;
+        }
     }
 }
