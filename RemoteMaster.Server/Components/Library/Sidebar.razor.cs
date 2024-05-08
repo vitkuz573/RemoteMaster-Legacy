d438eea782100651e90c998e6d89de3f4e619f02
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-08 07:41:55 +0500

    UPD

--- RemoteMaster.Server/Components/Library/Sidebar.razor.cs
+++ RemoteMaster.Server/Components/Library/Sidebar.razor.cs
@@ -2,6 +2,7 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using System.Globalization;
 using Microsoft.AspNetCore.Components;
 
 namespace RemoteMaster.Server.Components.Library;
@@ -20,6 +21,9 @@ public partial class Sidebar
     [Parameter]
     public string IconClosed { get; set; } = "chevron_left";
 
+    /// <summary>
+    /// Base color class used for both the sidebar and its toggle switch.
+    /// </summary>
     [Parameter]
     public string BaseColorClass { get; set; } = "bg-gray-800 text-white";
 
@@ -35,6 +39,18 @@ public partial class Sidebar
     [Parameter]
     public int AnimationDurationMs { get; set; } = 500;
 
+    /// <summary>
+    /// Opacity of the toggle switch when the sidebar is open, ranging from 0 (fully transparent) to 1 (fully opaque).
+    /// </summary>
+    [Parameter]
+    public double SwitchOpacityOpen { get; set; } = 1.0;
+
+    /// <summary>
+    /// Opacity of the toggle switch when the sidebar is closed, ranging from 0 (fully transparent) to 1 (fully opaque).
+    /// </summary>
+    [Parameter]
+    public double SwitchOpacityClosed { get; set; } = 0.5;
+
     private bool _isSidebarOpen;
 
     protected override void OnInitialized()
@@ -48,5 +64,13 @@ public partial class Sidebar
         {
             _isSidebarOpen = !_isSidebarOpen;
         }
+    }
+
+    /// <summary>
+    /// Gets the appropriate switch opacity based on the sidebar's current state.
+    /// </summary>
+    private string GetSwitchOpacity()
+    {
+        return (_isSidebarOpen ? SwitchOpacityOpen : SwitchOpacityClosed).ToString("0.##", CultureInfo.InvariantCulture);
     }
 }
