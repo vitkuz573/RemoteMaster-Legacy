9966f5133928b16da241e1038f7960d5a59671a5
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-09 12:54:23 +0500

    UPD

--- RemoteMaster.Server/Components/Library/Button.razor.cs
+++ RemoteMaster.Server/Components/Library/Button.razor.cs
@@ -9,45 +9,30 @@ namespace RemoteMaster.Server.Components.Library;
 
 public partial class Button
 {
-    /// <summary>
-    /// Text displayed on the button.
-    /// </summary>
     [Parameter]
     public string Label { get; set; } = "Button";
 
-    /// <summary>
-    /// Additional CSS classes for styling the button using Tailwind CSS.
-    /// </summary>
     [Parameter]
     public string CssClasses { get; set; } = "rounded-lg bg-blue-600 px-4 py-2 font-semibold text-white hover:bg-blue-700";
 
-    /// <summary>
-    /// Indicates if the button is disabled.
-    /// </summary>
     [Parameter]
     public bool IsDisabled { get; set; } = false;
 
-    /// <summary>
-    /// Icon displayed to the left of the button text.
-    /// </summary>
     [Parameter]
     public string Icon { get; set; } = "";
 
-    /// <summary>
-    /// Specifies the button type (submit, reset, button).
-    /// </summary>
+    [Parameter]
+    public string IconClasses { get; set; } = "material-icons mr-2";
+
     [Parameter]
     public ButtonType ButtonType { get; set; } = ButtonType.Button;
 
-    /// <summary>
-    /// Event callback triggered on button click.
-    /// </summary>
     [Parameter]
     public EventCallback OnClick { get; set; }
 
-    /// <summary>
-    /// Handles button click if the button is not disabled.
-    /// </summary>
+    [Parameter]
+    public bool IsToggled { get; set; } = false;
+
     private async Task HandleClick()
     {
         if (!IsDisabled)
@@ -62,14 +47,11 @@ public partial class Button
 
         if (IsDisabled)
         {
-            classes += " bg-gray-400 hover:bg-gray-400 border-gray-500 cursor-not-allowed opacity-50";
+            classes += " bg-gray-400 hover:bg-gray-400 cursor-not-allowed opacity-50";
         }
 
         return classes;
     }
 
-    private string GetButtonTypeAsString()
-    {
-        return ButtonType.ToString().ToLower();
-    }
+    private string GetButtonTypeAsString() => ButtonType.ToString().ToLower();
 }
