db631c1326e69ba5808a814851cde754a40e1669
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-09 12:19:50 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Library/Button.razor.cs
@@ -0,0 +1,24 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+
+namespace RemoteMaster.Server.Components.Library;
+
+public partial class Button
+{
+    [Parameter]
+    public string Label { get; set; } = "Button";
+
+    [Parameter]
+    public string CssClasses { get; set; } = "rounded bg-blue-600 px-4 py-2 font-semibold text-white hover:bg-blue-700";
+
+    [Parameter]
+    public EventCallback OnClick { get; set; }
+
+    private async Task HandleClick()
+    {
+        await OnClick.InvokeAsync();
+    }
+}
