18eefaf06c9d7095624dd47fab8e3b3746c78490
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-11 21:21:48 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Library/Overlay.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace RemoteMaster.Server.Components.Library;
+
+public partial class Overlay
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    private string OverlayClass => $"fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center {AdditionalClasses}";
+
+    [Parameter]
+    public string AdditionalClasses { get; set; } = string.Empty;
+}
