213540d285c53548678d8419eddaaccdf98855de
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-15 09:43:25 +0500

    UPD

--- RemoteMaster.Server/wwwroot/js/preventDrawer.js
+++
@@ -1,7 +0,0 @@
-﻿function preventDefaultForKeydownWhenDrawerClosed(drawerOpen) {
-    document.addEventListener("keydown", function (event) {
-        if ((event.key === "Enter" || event.key === " " || event.key === "Spacebar") && !drawerOpen) {
-            event.preventDefault();
-        }
-    });
-}
