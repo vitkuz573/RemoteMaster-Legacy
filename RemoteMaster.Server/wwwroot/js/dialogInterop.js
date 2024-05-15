40621ccfcbec36d628f4ac7ee7c9952259ba4d76
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-05-15 09:43:07 +0500

    UPD

--- RemoteMaster.Server/wwwroot/js/dialogInterop.js
+++
@@ -1,5 +0,0 @@
-﻿window.addGlobalKeydownListener = function (dotNetHelper) {
-    window.addEventListener('keydown', function (event) {
-        dotNetHelper.invokeMethodAsync('HandleKeyDown', event.key);
-    });
-}
