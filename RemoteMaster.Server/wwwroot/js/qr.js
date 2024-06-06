694b79e149d608f1d9949e0694a63db2f454bb6f
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-06-06 09:00:02 +0500

    UPD

---
+++ RemoteMaster.Server/wwwroot/js/qr.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener("load", () => {
+    const qrCodeElement = document.getElementById("qrCode");
+    if (qrCodeElement) {
+        const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+        new QRCode(qrCodeElement, {
+            text: uri,
+            width: 150,
+            height: 150
+        });
+    }
+});
