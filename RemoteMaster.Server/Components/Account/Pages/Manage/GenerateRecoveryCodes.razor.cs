00247e87563a560067d7641490d90a5e1d98f874
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-06-06 08:39:39 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Account/Pages/Manage/GenerateRecoveryCodes.razor.cs
@@ -0,0 +1,40 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using RemoteMaster.Server.Data;
+using Serilog;
+
+namespace RemoteMaster.Server.Components.Account.Pages.Manage;
+
+public partial class GenerateRecoveryCodes
+{
+    private string? _message;
+    private ApplicationUser _user = default!;
+    private IEnumerable<string>? _recoveryCodes;
+
+    [CascadingParameter]
+    private HttpContext HttpContext { get; set; } = default!;
+
+    protected async override Task OnInitializedAsync()
+    {
+        _user = await UserAccessor.GetRequiredUserAsync(HttpContext);
+
+        var isTwoFactorEnabled = await UserManager.GetTwoFactorEnabledAsync(_user);
+
+        if (!isTwoFactorEnabled)
+        {
+            throw new InvalidOperationException("Cannot generate recovery codes for user because they do not have 2FA enabled.");
+        }
+    }
+
+    private async Task OnSubmitAsync()
+    {
+        var userId = await UserManager.GetUserIdAsync(_user);
+        _recoveryCodes = await UserManager.GenerateNewTwoFactorRecoveryCodesAsync(_user, 10);
+        _message = "You have generated new recovery codes.";
+
+        Log.Information("User with ID '{UserId}' has generated new 2FA recovery codes.", userId);
+    }
+}
