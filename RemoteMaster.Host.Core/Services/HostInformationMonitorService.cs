c4be21da332695e357be0ccd5b53510239cc513d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-06-20 15:06:20 +0500

    UPD

--- RemoteMaster.Host.Core/Services/HostInformationMonitorService.cs
+++ RemoteMaster.Host.Core/Services/HostInformationMonitorService.cs
@@ -36,10 +36,7 @@ public class HostInformationMonitorService(IServerHubService serverHubService, I
         {
             if (hostConfiguration.Host != null && hostConfiguration.Host.MacAddress != hostInformation.MacAddress)
             {
-                Log.Information("MAC address has changed, which might indicate restoration from backup. System will be registered under a special organizational unit.");
-
-                hostConfiguration.Subject.Organization = "Restoration Systems";
-                hostConfiguration.Subject.OrganizationalUnit = ["Backup Recovery"];
+                Log.Information("MAC address has changed, which might indicate restoration from backup.");
             }
 
             hostConfiguration.Host = hostInformation;
