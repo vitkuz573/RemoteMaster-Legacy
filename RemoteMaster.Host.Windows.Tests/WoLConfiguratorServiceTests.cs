2b73158ccdb39764d5edd0f8935c4542201ff8b6
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-06-20 23:40:52 +0500

    UPD

--- RemoteMaster.Host.Windows.Tests/WoLConfiguratorServiceTests.cs
+++ RemoteMaster.Host.Windows.Tests/WoLConfiguratorServiceTests.cs
@@ -12,60 +12,56 @@ namespace RemoteMaster.Host.Windows.Tests;
 
 public class WoLConfiguratorServiceTests
 {
+    private readonly Mock<IRegistryService> _mockRegistryService;
+    private readonly Mock<IProcessService> _mockProcessService;
+    private readonly WoLConfiguratorService _service;
+
+    public WoLConfiguratorServiceTests()
+    {
+        _mockRegistryService = new Mock<IRegistryService>();
+        _mockProcessService = new Mock<IProcessService>();
+        _service = new WoLConfiguratorService(_mockRegistryService.Object, _mockProcessService.Object);
+    }
+
     [Fact]
     public void DisableFastStartup_SetsRegistryValue()
     {
-        // Arrange
-        var mockRegistryService = new Mock<IRegistryService>();
-        var mockProcessService = new Mock<IProcessService>();
-        var service = new WoLConfiguratorService(mockRegistryService.Object, mockProcessService.Object);
-
         // Act
-        service.DisableFastStartup();
+        _service.DisableFastStartup();
 
         // Assert
-        mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Session Manager\Power", "HiberbootEnabled", 0, RegistryValueKind.DWord), Times.Once);
+        _mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Session Manager\Power", "HiberbootEnabled", 0, RegistryValueKind.DWord), Times.Once);
     }
 
     [Fact]
     public void DisablePnPEnergySaving_SetsRegistryValuesForAllAdapters()
     {
         // Arrange
-        var mockRegistryService = new Mock<IRegistryService>();
-        var mockProcessService = new Mock<IProcessService>();
         var mockKey = new Mock<IRegistryKey>();
-
-        mockRegistryService.Setup(r => r.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}", true)).Returns(mockKey.Object);
+        _mockRegistryService.Setup(r => r.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}", true)).Returns(mockKey.Object);
         mockKey.Setup(k => k.GetSubKeyNames()).Returns(["0001", "0002"]);
 
-        var service = new WoLConfiguratorService(mockRegistryService.Object, mockProcessService.Object);
-
         // Act
-        service.DisablePnPEnergySaving();
+        _service.DisablePnPEnergySaving();
 
         // Assert
-        mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\0001", "PnPCapabilities", 0, RegistryValueKind.DWord), Times.Once);
-        mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\0002", "PnPCapabilities", 0, RegistryValueKind.DWord), Times.Once);
+        _mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\0001", "PnPCapabilities", 0, RegistryValueKind.DWord), Times.Once);
+        _mockRegistryService.Verify(r => r.SetValue(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\0002", "PnPCapabilities", 0, RegistryValueKind.DWord), Times.Once);
     }
 
     [Fact]
     public void EnableWakeOnLanForAllAdapters_EnablesWakeOnLanForAllDevices()
     {
         // Arrange
-        var mockRegistryService = new Mock<IRegistryService>();
-        var mockProcessService = new Mock<IProcessService>();
         var mockProcess = new Mock<Process>();
-
-        mockProcessService.Setup(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/devicequery wake_programmable"))).Returns(mockProcess.Object);
-        mockProcessService.Setup(p => p.ReadStandardOutput(mockProcess.Object)).Returns("Device1\r\nDevice2");
-
-        var service = new WoLConfiguratorService(mockRegistryService.Object, mockProcessService.Object);
+        _mockProcessService.Setup(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/devicequery wake_programmable"))).Returns(mockProcess.Object);
+        _mockProcessService.Setup(p => p.ReadStandardOutput(mockProcess.Object)).Returns("Device1\r\nDevice2");
 
         // Act
-        service.EnableWakeOnLanForAllAdapters();
+        _service.EnableWakeOnLanForAllAdapters();
 
         // Assert
-        mockProcessService.Verify(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/deviceenablewake \"Device1\"")), Times.Once);
-        mockProcessService.Verify(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/deviceenablewake \"Device2\"")), Times.Once);
+        _mockProcessService.Verify(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/deviceenablewake \"Device1\"")), Times.Once);
+        _mockProcessService.Verify(p => p.Start(It.Is<ProcessStartInfo>(info => info.FileName == "powercfg.exe" && info.Arguments == "/deviceenablewake \"Device2\"")), Times.Once);
     }
 }
