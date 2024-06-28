cf12b889feb931e1e76d0603a3f7d4759150bd20
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-06-28 12:08:16 +0500

    UPD

---
+++ RemoteMaster.Host.Windows.Tests/ScreenCapturerServiceTests.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Drawing;
+using Moq;
+using RemoteMaster.Host.Windows.Abstractions;
+
+namespace RemoteMaster.Host.Windows.Tests;
+
+public class ScreenCapturerServiceTests : IDisposable
+{
+    private readonly Mock<IDesktopService> _mockDesktopService;
+    private readonly TestScreenCapturerService _screenCapturerService;
+
+    public ScreenCapturerServiceTests()
+    {
+        _mockDesktopService = new Mock<IDesktopService>();
+        _screenCapturerService = new TestScreenCapturerService(_mockDesktopService.Object);
+    }
+
+    [Fact]
+    public void GetDisplays_ShouldReturnDisplays()
+    {
+        var displays = _screenCapturerService.GetDisplays();
+
+        Assert.NotNull(displays);
+        var display = displays.First();
+        Assert.Equal("Test Display", display.Name);
+        Assert.True(display.IsPrimary);
+        Assert.Equal(new Size(1920, 1080), display.Resolution);
+    }
+
+    [Fact]
+    public void SetSelectedScreen_ShouldSetSelectedScreen()
+    {
+        _screenCapturerService.SetSelectedScreen("New Display");
+
+        Assert.Equal("New Display", _screenCapturerService.SelectedScreen);
+    }
+
+    [Fact]
+    public void GetNextFrame_ShouldReturnFrame()
+    {
+        _mockDesktopService.Setup(ds => ds.SwitchToInputDesktop()).Returns(true);
+
+        var frame = _screenCapturerService.GetNextFrame();
+
+        Assert.NotNull(frame);
+    }
+
+    [Fact]
+    public void GetThumbnail_ShouldReturnThumbnail()
+    {
+        _mockDesktopService.Setup(ds => ds.SwitchToInputDesktop()).Returns(true);
+
+        var thumbnail = _screenCapturerService.GetThumbnail(100, 100);
+
+        Assert.NotNull(thumbnail);
+    }
+
+    public void Dispose()
+    {
+        _screenCapturerService?.Dispose();
+    }
+}
---
+++ RemoteMaster.Host.Windows.Tests/TestScreenCapturerService.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Drawing;
+using RemoteMaster.Host.Windows.Abstractions;
+using RemoteMaster.Host.Windows.Services;
+using RemoteMaster.Shared.Models;
+
+namespace RemoteMaster.Host.Windows.Tests;
+
+public class TestScreenCapturerService : ScreenCapturerService
+{
+    public TestScreenCapturerService(IDesktopService desktopService) : base(desktopService)
+    {
+    }
+
+    protected override void Init()
+    {
+        // Implementation for testing
+    }
+
+    protected override byte[]? GetFrame()
+    {
+        // Create a simple 1x1 red pixel image for testing
+        using var bitmap = new Bitmap(1, 1);
+        using var graphics = Graphics.FromImage(bitmap);
+        graphics.Clear(Color.Red);
+
+        using var ms = new MemoryStream();
+        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+        return ms.ToArray();
+    }
+
+    public override IEnumerable<Display> GetDisplays()
+    {
+        return
+        [
+            new()
+            {
+                Name = "Test Display",
+                IsPrimary = true,
+                Resolution = new Size(1920, 1080)
+            }
+        ];
+    }
+
+    public override void SetSelectedScreen(string displayName)
+    {
+        SelectedScreen = displayName;
+    }
+
+    protected override void RefreshCurrentScreenBounds()
+    {
+        CurrentScreenBounds = new Rectangle(0, 0, 1920, 1080);
+    }
+
+    public override Rectangle CurrentScreenBounds { get; protected set; }
+
+    public override Rectangle VirtualScreenBounds => new(0, 0, 3840, 1080);
+
+    public override string SelectedScreen { get; protected set; } = "Test Display";
+
+    protected override bool HasMultipleScreens => true;
+}
