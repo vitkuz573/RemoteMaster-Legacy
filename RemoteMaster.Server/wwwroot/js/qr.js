6e9ba78d920edb86f4f0b11dff89c58f86aecaf0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-05 13:34:40 +0500

    UPD

--- RemoteMaster.Server/wwwroot/js/qr.js
+++ RemoteMaster.Server/wwwroot/js/qr.js
@@ -1,11 +1,23 @@
-﻿window.addEventListener("load", () => {
-    const qrCodeElement = document.getElementById("qrCode");
-    if (qrCodeElement) {
-        const uri = document.getElementById("qrCodeData").getAttribute('data-url');
-        new QRCode(qrCodeElement, {
-            text: uri,
-            width: 150,
-            height: 150
-        });
-    }
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const targetNode = document.querySelector('body');
+    const config = { childList: true, subtree: true };
+
+    const callback = function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const qrCodeElement = document.getElementById("qrCode");
+                if (qrCodeElement && !qrCodeElement.hasChildNodes()) {
+                    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+                    new QRCode(qrCodeElement, {
+                        text: uri,
+                        width: 150,
+                        height: 150
+                    });
+                }
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
 });
