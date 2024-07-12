eeee57ed082c7a6eeecdb0f39b5526b5e90e536c
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-12 14:44:52 +0500

    UPD

--- RemoteMaster.Host.Core/Services/AppState.cs
+++ RemoteMaster.Host.Core/Services/AppState.cs
@@ -12,65 +12,94 @@ namespace RemoteMaster.Host.Core.Services;
 
 public class AppState(IHubContext<ControlHub, IControlClient> hubContext) : IAppState
 {
-    public event EventHandler<IViewer>? ViewerAdded;
+    private readonly ConcurrentDictionary<string, IViewer> _viewers = new();
+    private static readonly object Lock = new();
 
+    public event EventHandler<IViewer>? ViewerAdded;
     public event EventHandler<IViewer?>? ViewerRemoved;
 
-    private readonly ConcurrentDictionary<string, IViewer> _viewers = new();
-
-    public IReadOnlyDictionary<string, IViewer> Viewers => _viewers;
+    public IReadOnlyDictionary<string, IViewer> Viewers
+    {
+        get
+        {
+            lock (Lock)
+            {
+                return new Dictionary<string, IViewer>(_viewers);
+            }
+        }
+    }
 
     public bool TryGetViewer(string connectionId, out IViewer? viewer)
     {
-        return _viewers.TryGetValue(connectionId, out viewer);
+        lock (Lock)
+        {
+            return _viewers.TryGetValue(connectionId, out viewer);
+        }
     }
 
     public bool TryAddViewer(IViewer viewer)
     {
-        ArgumentNullException.ThrowIfNull(viewer);
-
-        var result = _viewers.TryAdd(viewer.ConnectionId, viewer);
-
-        if (!result)
+        lock (Lock)
         {
+            ArgumentNullException.ThrowIfNull(viewer);
+
+            var result = _viewers.TryAdd(viewer.ConnectionId, viewer);
+
+            if (!result)
+            {
+                return result;
+            }
+
+            ViewerAdded?.Invoke(this, viewer);
+            NotifyViewersChanged();
+
             return result;
         }
-
-        ViewerAdded?.Invoke(this, viewer);
-        NotifyViewersChanged();
-
-        return result;
     }
 
     public bool TryRemoveViewer(string connectionId)
     {
+        lock (Lock)
+        {
 #pragma warning disable CA2000
-        var result = _viewers.TryRemove(connectionId, out var viewer);
+            var result = _viewers.TryRemove(connectionId, out var viewer);
 #pragma warning restore CA2000
 
-        if (!result)
-        {
+            if (!result)
+            {
+                return result;
+            }
+
+            try
+            {
+                ViewerRemoved?.Invoke(this, viewer);
+                NotifyViewersChanged();
+            }
+            finally
+            {
+                viewer?.Dispose();
+            }
+
             return result;
         }
-
-        try
-        {
-            ViewerRemoved?.Invoke(this, viewer);
-            NotifyViewersChanged();
-        }
-        finally
-        {
-            viewer?.Dispose();
-        }
-
-        return result;
     }
 
-    public IReadOnlyList<IViewer> GetAllViewers() => [.. _viewers.Values];
+    public IReadOnlyList<IViewer> GetAllViewers()
+    {
+        lock (Lock)
+        {
+            return [.. _viewers.Values];
+        }
+    }
 
     private void NotifyViewersChanged()
     {
-        var viewers = GetAllViewers().Select(v => new ViewerDto(v.ConnectionId, v.Group, v.UserName, v.Role, v.ConnectedTime)).ToList();
+        List<ViewerDto> viewers;
+
+        lock (Lock)
+        {
+            viewers = GetAllViewers().Select(v => new ViewerDto(v.ConnectionId, v.Group, v.UserName, v.Role, v.ConnectedTime)).ToList();
+        }
 
         hubContext.Clients.All.ReceiveAllViewers(viewers);
     }
