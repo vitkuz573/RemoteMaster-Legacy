d0641406323151cb83568b412f8c630e56f6efd0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-15 10:55:59 +0500

    UPD

--- RemoteMaster.Server/Controllers/V1/HostMoveRequestController.cs
+++ RemoteMaster.Server/Controllers/V1/HostMoveRequestController.cs
@@ -2,6 +2,7 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using RemoteMaster.Server.Abstractions;
 using RemoteMaster.Shared.Models;
@@ -11,12 +12,15 @@ namespace RemoteMaster.Server.Controllers.V1;
 
 [ApiController]
 [Route("api/[controller]")]
+[ApiVersion("1.0")]
+[Consumes("application/vnd.remotemaster.v1+json")]
+[Produces("application/vnd.remotemaster.v1+json")]
 public class HostMoveRequestController(IHostMoveRequestService hostMoveRequestService) : ControllerBase
 {
     [HttpGet]
     [SwaggerOperation(Summary = "Retrieves a host move request", Description = "Retrieves a host move request by MAC address.")]
     [ProducesResponseType(typeof(ApiResponse<HostMoveRequest>), 200)]
-    [Produces("application/json")]
+    [ProducesResponseType(typeof(ApiResponse<HostMoveRequest>), 400)]
     public async Task<IActionResult> GetHostMoveRequest([FromQuery] string macAddress)
     {
         var hostMoveRequest = await hostMoveRequestService.GetHostMoveRequestAsync(macAddress);
@@ -33,13 +37,12 @@ public class HostMoveRequestController(IHostMoveRequestService hostMoveRequestSe
     [SwaggerOperation(Summary = "Acknowledges a host move request", Description = "Acknowledges a host move request by removing it from the list.")]
     [ProducesResponseType(typeof(ApiResponse<bool>), 200)]
     [ProducesResponseType(typeof(ApiResponse<bool>), 400)]
-    [Produces("application/json")]
     public async Task<IActionResult> AcknowledgeMoveRequest([FromBody] string macAddress)
     {
         try
         {
             await hostMoveRequestService.AcknowledgeMoveRequestAsync(macAddress);
-
+            
             return Ok(ApiResponse<bool>.Success(true, "Host move request acknowledged successfully."));
         }
         catch (Exception ex)
