38b29067b1092bf3de22306665bb44cfdf36b763
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-15 10:58:11 +0500

    UPD

--- RemoteMaster.Server/Controllers/V1/CertificateController.cs
+++ RemoteMaster.Server/Controllers/V1/CertificateController.cs
@@ -3,6 +3,7 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using System.Security.Cryptography.X509Certificates;
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using RemoteMaster.Server.Abstractions;
 using RemoteMaster.Shared.Models;
@@ -12,13 +13,15 @@ namespace RemoteMaster.Server.Controllers.V1;
 
 [ApiController]
 [Route("api/[controller]")]
+[ApiVersion("1.0")]
+[Consumes("application/vnd.remotemaster.v1+json")]
+[Produces("application/vnd.remotemaster.v1+json")]
 public class CertificateController(ICertificateService certificateService) : ControllerBase
 {
     [HttpPost("issue")]
     [SwaggerOperation(Summary = "Issues a certificate", Description = "Issues a certificate based on the provided CSR bytes.")]
     [ProducesResponseType(typeof(ApiResponse<byte[]>), 200)]
     [ProducesResponseType(typeof(ApiResponse<byte[]>), 400)]
-    [Produces("application/json")]
     public IActionResult IssueCertificate([FromBody] byte[] csrBytes)
     {
         try
