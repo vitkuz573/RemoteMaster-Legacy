e7139acb02fd502b1063f1f3ec92dee3e3a549de
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-16 09:43:36 +0500

    UPD

--- RemoteMaster.Server/Validators/ValidateMediaTypeAttribute.cs
+++ RemoteMaster.Server/Validators/ValidateMediaTypeAttribute.cs
@@ -2,8 +2,9 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 
 namespace RemoteMaster.Server.Validators;
@@ -26,11 +27,22 @@ public class ValidateMediaTypeAttribute : ActionFilterAttribute
             if (!_supportedMediaTypes.Contains(mediaType))
             {
                 context.HttpContext.Response.StatusCode = StatusCodes.Status415UnsupportedMediaType;
-                context.Result = new Microsoft.AspNetCore.Mvc.JsonResult(new
+                context.HttpContext.Response.ContentType = "application/json";
+
+                var response = new
                 {
                     Status = StatusCodes.Status415UnsupportedMediaType,
                     Message = $"The media type '{mediaType}' is not supported."
+                };
+
+                var jsonResponse = JsonSerializer.Serialize(response, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
                 });
+
+                context.HttpContext.Response.WriteAsync(jsonResponse);
+                context.Result = new EmptyResult();
 
                 return;
             }
