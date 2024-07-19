b19a7c72123fbf3fb63336d7b27f7e186772c4a1
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-19 13:51:42 +0500

    UPD

---
+++ RemoteMaster.Server.Tests/WakeOnLanServiceTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Net;
+using Moq;
+using RemoteMaster.Server.Abstractions;
+using RemoteMaster.Server.Helpers;
+using RemoteMaster.Server.Services;
+
+namespace RemoteMaster.Server.Tests;
+
+public class WakeOnLanServiceTests
+{
+    [Fact]
+    public void WakeUp_SendsMagicPacket()
+    {
+        // Arrange
+        var packetSenderMock = new Mock<IPacketSender>();
+        var wakeOnLanService = new WakeOnLanService(packetSenderMock.Object);
+        const string macAddress = "01:23:45:67:89:AB";
+        const int port = 9;
+        var expectedPacket = MagicPacketCreator.Create(macAddress);
+        var expectedEndPoint = new IPEndPoint(IPAddress.Broadcast, port);
+
+        // Act
+        wakeOnLanService.WakeUp(macAddress, port);
+
+        // Assert
+        packetSenderMock.Verify(ps => ps.Send(It.Is<byte[]>(packet => packet.SequenceEqual(expectedPacket)),
+                It.Is<IPEndPoint>(ep => ep.Equals(expectedEndPoint))),
+            Times.Once);
+    }
+
+    [Fact]
+    public void WakeUp_DefaultPort()
+    {
+        // Arrange
+        var packetSenderMock = new Mock<IPacketSender>();
+        var wakeOnLanService = new WakeOnLanService(packetSenderMock.Object);
+        const string macAddress = "01:23:45:67:89:AB";
+        const int defaultPort = 9;
+        var expectedPacket = MagicPacketCreator.Create(macAddress);
+        var expectedEndPoint = new IPEndPoint(IPAddress.Broadcast, defaultPort);
+
+        // Act
+        wakeOnLanService.WakeUp(macAddress);
+
+        // Assert
+        packetSenderMock.Verify(ps => ps.Send(It.Is<byte[]>(packet => packet.SequenceEqual(expectedPacket)),
+                It.Is<IPEndPoint>(ep => ep.Equals(expectedEndPoint))),
+            Times.Once);
+    }
+}
