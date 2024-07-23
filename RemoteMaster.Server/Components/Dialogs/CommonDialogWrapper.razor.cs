0d8a17558f31196f84f2a5acf25b4a4c07da5521
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-23 15:33:27 +0500

    UPD

---
+++ RemoteMaster.Server/Components/Dialogs/CommonDialogWrapper.razor.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using RemoteMaster.Shared.Models;
+
+namespace RemoteMaster.Server.Components.Dialogs;
+
+public partial class CommonDialogWrapper<TDialog> where TDialog : ComponentBase
+{
+    [Parameter]
+    public ConcurrentDictionary<Computer, HubConnection?> Hosts { get; set; } = default!;
+
+    [Parameter]
+    public string HubPath { get; set; } = default!;
+
+    [Parameter]
+    public bool StartConnection { get; set; }
+
+    [Parameter]
+    public bool RequireConnections { get; set; }
+
+    [Parameter]
+    public IDictionary<string, object> AdditionalParameters { get; set; } = new Dictionary<string, object>();
+}
