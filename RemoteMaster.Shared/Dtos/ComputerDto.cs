3304c58cd5d71baff2b3cc0b947f7fee549900c7
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-28 18:51:52 +0500

    UPD

---
+++ RemoteMaster.Shared/Dtos/ComputerDto.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using System.Text.Json.Serialization;
+
+namespace RemoteMaster.Shared.Dtos;
+
+public class ComputerDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("ipAddress")]
+    public string IpAddress { get; set; } = string.Empty;
+
+    [JsonPropertyName("macAddress")]
+    public string MacAddress { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    public byte[]? Thumbnail { get; set; }
+
+    [JsonIgnore]
+    public Guid? ParentId { get; set; }
+
+    [JsonIgnore]
+    public ComputerDto? Parent { get; set; }
+}
