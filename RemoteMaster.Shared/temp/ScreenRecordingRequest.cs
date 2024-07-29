1bf64f47467bab7950a47672f6b84e16a1513ffd
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-29 08:43:58 +0500

    Rename Dtos to temp

---
+++ RemoteMaster.Shared/temp/ScreenRecordingRequest.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+namespace RemoteMaster.Shared.DTOs;
+
+public class ScreenRecordingRequest(string output)
+{
+    public string Output { get; } = output;
+
+    public uint Duration { get; init; }
+
+    public uint VideoQuality { get; init; }
+}
