46190f82a74886d524f890a4ac90b7cf1862a219
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-07-29 08:49:18 +0500

    Rename temp to DTOs

---
+++ RemoteMaster.Shared/DTOs/DomainJoinRequest.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using RemoteMaster.Shared.Models;
+
+namespace RemoteMaster.Shared.DTOs;
+
+public class DomainJoinRequest(string domain, Credentials userCredentials)
+{
+    public string Domain { get; } = domain;
+
+    public Credentials UserCredentials { get; } = userCredentials;
+}
+
---
+++ RemoteMaster.Shared/DTOs/PowerActionRequest.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+namespace RemoteMaster.Shared.DTOs;
+
+public class PowerActionRequest
+{
+    public string? Message { get; init; }
+
+    public uint Timeout { get; init; }
+
+    public bool ForceAppsClosed { get; init; }
+}
+
