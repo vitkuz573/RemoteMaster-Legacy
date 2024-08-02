233dd5084814e1d9eb46e8ee1a39016a59898ae0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-08-02 11:55:27 +0500

    UPD

--- RemoteMaster.Host.Core/Hubs/ChatHub.cs
+++ RemoteMaster.Host.Core/Hubs/ChatHub.cs
@@ -2,14 +2,34 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
 
 namespace RemoteMaster.Host.Core.Hubs;
 
 public class ChatHub : Hub
 {
+    private static readonly ConcurrentQueue<(string User, string Message)> Messages = new();
+
     public async Task SendMessage(string user, string message)
     {
+        Messages.Enqueue((user, message));
+
+        while (Messages.Count > 100)
+        {
+            Messages.TryDequeue(out _);
+        }
+
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async override Task OnConnectedAsync()
+    {
+        foreach (var (user, message) in Messages)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
+
+        await base.OnConnectedAsync();
     }
 }
