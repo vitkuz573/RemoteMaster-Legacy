79115aad471cb4211c5ec78b70e90776e10ba0de
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-08-25 18:12:41 +0500

    UPD

--- RemoteMaster.Server/ValueObjects/Address.cs
+++ RemoteMaster.Server/ValueObjects/Address.cs
@@ -2,15 +2,31 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+using System.Globalization;
+
 namespace RemoteMaster.Server.ValueObjects;
 
 public class Address(string locality, string state, string country)
 {
     public string Locality { get; } = locality ?? throw new ArgumentNullException(nameof(locality));
-    
+
     public string State { get; } = state ?? throw new ArgumentNullException(nameof(state));
-    
-    public string Country { get; } = country ?? throw new ArgumentNullException(nameof(country));
+
+    public string Country { get; } = IsValidCountryCode(country)
+        ? country
+        : throw new ArgumentException("Invalid country code", nameof(country));
+
+    private static bool IsValidCountryCode(string countryCode)
+    {
+        if (string.IsNullOrEmpty(countryCode) || countryCode.Length != 2)
+        {
+            return false;
+        }
+
+        return CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+            .Select(culture => new RegionInfo(culture.Name))
+            .Any(ri => ri.TwoLetterISORegionName.Equals(countryCode, StringComparison.OrdinalIgnoreCase));
+    }
 
     public override bool Equals(object? obj) =>
         obj is Address address &&
