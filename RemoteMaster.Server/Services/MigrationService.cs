04c56d463165af7caf3262d3e5fcdf2374e294e1
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-09-09 07:34:52 +0500

    UPD

--- RemoteMaster.Server/Services/MigrationService.cs
+++ RemoteMaster.Server/Services/MigrationService.cs
@@ -2,7 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-
 using Microsoft.EntityFrameworkCore;
 using RemoteMaster.Server.Data;
 using Serilog;
@@ -11,31 +10,27 @@ namespace RemoteMaster.Server.Services;
 
 public class MigrationService(IServiceScopeFactory serviceScopeFactory) : IHostedService
 {
-    public Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
         using var scope = serviceScopeFactory.CreateScope();
-        var applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        var certificateDbContext = scope.ServiceProvider.GetRequiredService<CertificateDbContext>();
 
         var dbContexts = new List<DbContext>
         {
-            applicationDbContext,
-            certificateDbContext
+            scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(),
+            scope.ServiceProvider.GetRequiredService<CertificateDbContext>()
         };
 
         try
         {
             foreach (var dbContext in dbContexts)
             {
-                dbContext.Database.Migrate();
+                await dbContext.Database.MigrateAsync(cancellationToken);
             }
         }
         catch (Exception ex)
         {
             Log.Error(ex, "An error occurred while applying the database migrations.");
         }
-
-        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
