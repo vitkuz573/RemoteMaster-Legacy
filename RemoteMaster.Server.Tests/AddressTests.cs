fa145e39670d45fca59276753aa8fb25306b49f5
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-09-22 14:22:58 +0500

    UPD

---
+++ RemoteMaster.Server.Tests/AddressTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using RemoteMaster.Server.Aggregates.OrganizationAggregate.ValueObjects;
+
+namespace RemoteMaster.Server.Tests;
+
+public class AddressTests
+{
+    [Fact]
+    public void Address_CreatesCorrectly_WithValidArguments()
+    {
+        // Arrange
+        var locality = "New York";
+        var state = "NY";
+        var country = new CountryCode("US");
+
+        // Act
+        var address = new Address(locality, state, country);
+
+        // Assert
+        Assert.Equal(locality, address.Locality);
+        Assert.Equal(state, address.State);
+        Assert.Equal(country, address.Country);
+    }
+
+    [Fact]
+    public void Address_ThrowsArgumentNullException_WhenLocalityIsNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Address(null!, "NY", new CountryCode("US")));
+    }
+
+    [Fact]
+    public void Address_ThrowsArgumentNullException_WhenStateIsNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Address("New York", null!, new CountryCode("US")));
+    }
+
+    [Fact]
+    public void Address_ThrowsArgumentNullException_WhenCountryIsNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Address("New York", "NY", null!));
+    }
+
+    [Fact]
+    public void Address_Equals_ReturnsTrueForEqualAddresses()
+    {
+        // Arrange
+        var address1 = new Address("New York", "NY", new CountryCode("US"));
+        var address2 = new Address("New York", "NY", new CountryCode("US"));
+
+        // Act & Assert
+        Assert.True(address1.Equals(address2));
+    }
+
+    [Fact]
+    public void Address_Equals_ReturnsFalseForDifferentAddresses()
+    {
+        // Arrange
+        var address1 = new Address("New York", "NY", new CountryCode("US"));
+        var address2 = new Address("Los Angeles", "CA", new CountryCode("US"));
+
+        // Act & Assert
+        Assert.False(address1.Equals(address2));
+    }
+
+    [Fact]
+    public void Address_GetHashCode_ReturnsSameHashCodeForEqualAddresses()
+    {
+        // Arrange
+        var address1 = new Address("New York", "NY", new CountryCode("US"));
+        var address2 = new Address("New York", "NY", new CountryCode("US"));
+
+        // Act & Assert
+        Assert.Equal(address1.GetHashCode(), address2.GetHashCode());
+    }
+
+    [Fact]
+    public void Address_IsImmutable()
+    {
+        // Arrange
+        var address = new Address("New York", "NY", new CountryCode("US"));
+
+        // Act & Assert
+        var localityProperty = address.GetType().GetProperty(nameof(Address.Locality));
+        var stateProperty = address.GetType().GetProperty(nameof(Address.State));
+        var countryProperty = address.GetType().GetProperty(nameof(Address.Country));
+
+        Assert.NotNull(localityProperty);
+        Assert.NotNull(stateProperty);
+        Assert.NotNull(countryProperty);
+
+        Assert.Throws<ArgumentException>(() => localityProperty!.SetValue(address, "Los Angeles"));
+        Assert.Throws<ArgumentException>(() => stateProperty!.SetValue(address, "CA"));
+        Assert.Throws<ArgumentException>(() => countryProperty!.SetValue(address, new CountryCode("CA")));
+    }
+}
---
+++ RemoteMaster.Server.Tests/CountryCodeTests.cs
@@ -0,0 +1,78 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using RemoteMaster.Server.Aggregates.OrganizationAggregate.ValueObjects;
+
+namespace RemoteMaster.Server.Tests;
+
+public class CountryCodeTests
+{
+    [Fact]
+    public void CountryCode_CreatesCorrectly_WithValidCode()
+    {
+        // Arrange
+        var code = "US";
+
+        // Act
+        var countryCode = new CountryCode(code);
+
+        // Assert
+        Assert.Equal(code, countryCode.Code);
+    }
+
+    [Fact]
+    public void CountryCode_ThrowsArgumentException_WithInvalidCode()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new CountryCode("INVALID"));
+        Assert.Throws<ArgumentException>(() => new CountryCode(""));
+        Assert.Throws<ArgumentException>(() => new CountryCode(null!));
+    }
+
+    [Fact]
+    public void CountryCode_Equals_ReturnsTrueForEqualCountryCodes()
+    {
+        // Arrange
+        var countryCode1 = new CountryCode("US");
+        var countryCode2 = new CountryCode("US");
+
+        // Act & Assert
+        Assert.True(countryCode1.Equals(countryCode2));
+    }
+
+    [Fact]
+    public void CountryCode_Equals_ReturnsFalseForDifferentCountryCodes()
+    {
+        // Arrange
+        var countryCode1 = new CountryCode("US");
+        var countryCode2 = new CountryCode("CA");
+
+        // Act & Assert
+        Assert.False(countryCode1.Equals(countryCode2));
+    }
+
+    [Fact]
+    public void CountryCode_GetHashCode_ReturnsSameHashCodeForEqualCountryCodes()
+    {
+        // Arrange
+        var countryCode1 = new CountryCode("US");
+        var countryCode2 = new CountryCode("US");
+
+        // Act & Assert
+        Assert.Equal(countryCode1.GetHashCode(), countryCode2.GetHashCode());
+    }
+
+    [Fact]
+    public void CountryCode_IsImmutable()
+    {
+        // Arrange
+        var countryCode = new CountryCode("US");
+
+        // Act & Assert
+        var codeProperty = countryCode.GetType().GetProperty(nameof(CountryCode.Code));
+
+        Assert.NotNull(codeProperty);
+        Assert.Throws<ArgumentException>(() => codeProperty!.SetValue(countryCode, "CA"));
+    }
+}
