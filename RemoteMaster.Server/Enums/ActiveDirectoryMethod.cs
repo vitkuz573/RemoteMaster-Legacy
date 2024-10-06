90d212b0a2ffb1f976f1c6742a35cada26b1301a
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-10-06 12:15:21 +0500

    UPD

--- RemoteMaster.Server/Enums/ActiveDirectoryMethod.cs
+++ RemoteMaster.Server/Enums/ActiveDirectoryMethod.cs
@@ -6,5 +6,6 @@ namespace RemoteMaster.Server.Enums;
 
 public enum ActiveDirectoryMethod
 {
-    WebEnrollment
+    WebEnrollment,
+    CertEnroll
 }
--- RemoteMaster.Server/Services/CertificateService.cs
+++ RemoteMaster.Server/Services/CertificateService.cs
@@ -3,6 +3,7 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
@@ -14,6 +15,8 @@ using RemoteMaster.Server.Enums;
 using RemoteMaster.Server.Options;
 using RemoteMaster.Shared.Abstractions;
 using Serilog;
+using Windows.Win32.Foundation;
+using Windows.Win32.Security.Cryptography.Certificates;
 
 namespace RemoteMaster.Server.Services;
 
@@ -49,6 +52,8 @@ public class CertificateService(IHostInformationService hostInformationService, 
                 {
                     case ActiveDirectoryMethod.WebEnrollment:
                         return await IssueCertificateUsingWebEnrollment(csrBytes);
+                    case ActiveDirectoryMethod.CertEnroll:
+                        return IssueCertificateUsingCertEnroll(csrBytes);
                     default:
                         throw new InvalidOperationException("Unknown method.");
                 }
@@ -142,6 +147,95 @@ public class CertificateService(IHostInformationService hostInformationService, 
             var certBytes = await certResponse.Content.ReadAsByteArrayAsync();
 
             return new X509Certificate2(certBytes);
+        }
+    }
+
+    public Result<X509Certificate2> IssueCertificateUsingCertEnroll(byte[] csrBytes)
+    {
+        try
+        {
+            var certRequestPkcs10 = CreateInstance<IX509CertificateRequestPkcs10>("728AB342-217D-11DA-B2A4-000E7BBB2B09");
+
+            if (certRequestPkcs10 == null)
+            {
+                throw new InvalidOperationException("Failed to create IX509CertificateRequestPkcs10 instance.");
+            }
+
+            var csrBase64 = Convert.ToBase64String(csrBytes);
+            var bstrCsr = Marshal.StringToBSTR(csrBase64);
+
+            try
+            {
+                certRequestPkcs10.InitializeDecode((BSTR)bstrCsr, EncodingType.XCN_CRYPT_STRING_BASE64);
+            }
+            finally
+            {
+                Marshal.FreeBSTR(bstrCsr);
+            }
+
+            var enrollment = CreateInstance<IX509Enrollment>("728AB340-217D-11DA-B2A4-000E7BBB2B09");
+
+            if (enrollment == null)
+            {
+                throw new InvalidOperationException("Failed to create IX509Enrollment instance.");
+            }
+
+            enrollment.InitializeFromRequest(certRequestPkcs10);
+
+            var response = SendCertEnrollRequestToCA(enrollment);
+
+            var bstrResponse = Marshal.StringToBSTR(response);
+
+            try
+            {
+                var certBytes = Convert.FromBase64String(response);
+                var certificate = new X509Certificate2(certBytes);
+
+                return Result.Ok(certificate);
+            }
+            finally
+            {
+                Marshal.FreeBSTR(bstrResponse);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while issuing a certificate using CertEnroll.");
+
+            return Result.Fail<X509Certificate2>(new Error("An error occurred while issuing a certificate using CertEnroll.").CausedBy(ex));
+        }
+    }
+
+    private string SendCertEnrollRequestToCA(IX509Enrollment enrollment)
+    {
+        enrollment.Enroll();
+
+        unsafe
+        {
+            BSTR responseBstr;
+            enrollment.get_Response(EncodingType.XCN_CRYPT_STRING_BASE64, &responseBstr);
+
+            return responseBstr.ToString();
+        }
+    }
+
+    private static T? CreateInstance<T>(string clsid) where T : class
+    {
+        try
+        {
+            var type = Type.GetTypeFromCLSID(new Guid(clsid));
+
+            return type == null
+                ? throw new InvalidOperationException($"The type for CLSID {clsid} could not be found.")
+                : (T?)Activator.CreateInstance(type);
+        }
+        catch (COMException ex) when (ex.ErrorCode == unchecked((int)0x80040154))
+        {
+            throw new InvalidOperationException($"{typeof(T).Name} COM object is not registered. Please ensure that all necessary components are installed and registered correctly.", ex);
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"An unexpected error occurred while trying to create the {typeof(T).Name} instance.", ex);
         }
     }
 }
