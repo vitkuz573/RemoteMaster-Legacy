5bd7595e7ced170f127750cacd1c11f685fec7cb
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-10-07 10:09:07 +0500

    UPD

--- RemoteMaster.Server/Components/Admin/Dialogs/ConfirmationDialog.razor.cs
+++ RemoteMaster.Server/Components/Admin/Dialogs/ConfirmationDialog.razor.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
 
 namespace RemoteMaster.Server.Components.Admin.Dialogs;
 
 public partial class ConfirmationDialog
 {
     [Parameter]
-    public string Title { get; set; }
+    public string Title { get; set; } = default!;
 
     [Parameter]
-    public string Message { get; set; }
+    public string Message { get; set; } = default!;
 
 #pragma warning disable CA2227
     [Parameter]
-    public Dictionary<string, string> Parameters { get; set; }
+    public Dictionary<string, string> Parameters { get; set; } = default!;
 #pragma warning restore CA2227
 
     [Parameter]
