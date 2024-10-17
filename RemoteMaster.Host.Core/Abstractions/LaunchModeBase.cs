4a58fcd04a9ba7d1ba1803b574b79a096fca78df
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-10-17 09:24:49 +0500

    UPD

--- RemoteMaster.Host.Core/Abstractions/LaunchModeBase.cs
+++ RemoteMaster.Host.Core/Abstractions/LaunchModeBase.cs
@@ -2,11 +2,6 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
-
-// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
-// This file is part of the RemoteMaster project.
-// Licensed under the GNU Affero General Public License v3.0.
-
 namespace RemoteMaster.Host.Core.Abstractions;
 
 public abstract class LaunchModeBase
