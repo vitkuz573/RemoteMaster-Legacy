019fff62939bb6e167983571727575b6e9aecb09
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-11-01 12:29:04 +0500

    UPD

--- RemoteMaster.Host.Windows/ScreenOverlays/ClickIndicatorOverlay.cs
+++ RemoteMaster.Host.Windows/ScreenOverlays/ClickIndicatorOverlay.cs
@@ -3,19 +3,17 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using System.Drawing;
-using Microsoft.Extensions.Logging;
 using RemoteMaster.Host.Windows.Abstractions;
 
 namespace RemoteMaster.Host.Windows.ScreenOverlays;
 
-public class ClickIndicatorOverlay(ILogger<ClickIndicatorOverlay> logger) : IScreenOverlay
+public class ClickIndicatorOverlay : IScreenOverlay
 {
     private Point? _lastClickPosition;
 
     public void RegisterClick(Point position)
     {
         _lastClickPosition = position;
-        logger.LogInformation("Registered click at position: {Position}", position);
     }
 
     public void Draw(Graphics graphics, Rectangle screenBounds)
@@ -24,18 +22,14 @@ public class ClickIndicatorOverlay(ILogger<ClickIndicatorOverlay> logger) : IScr
 
         if (!_lastClickPosition.HasValue)
         {
-            logger.LogDebug("No click position to draw.");
             return;
         }
 
         using var brush = new SolidBrush(Color.Red);
         var drawPosition = new Point(_lastClickPosition.Value.X - 10, _lastClickPosition.Value.Y - 10);
 
-        logger.LogInformation("Drawing click indicator at position: {DrawPosition}", drawPosition);
-
         graphics.FillEllipse(brush, drawPosition.X, drawPosition.Y, 20, 20);
 
         _lastClickPosition = null;
-        logger.LogDebug("Reset last click position after drawing.");
     }
 }
