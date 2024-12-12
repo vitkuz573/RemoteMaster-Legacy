b7039cf57fc18fb34c6657efd8e4aeb5a447d3a8
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-12-12 12:53:41 +0500

    UPD

--- RemoteMaster.Server/tailwind.config.js
+++ RemoteMaster.Server/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./Components/**/*.{razor,razor.cs}", "./wwwroot/js/*.js", "./src/ts/*.ts"],
+    content: ["./Components/**/*.{razor,razor.cs,cs}", "./wwwroot/js/*.js", "./src/ts/*.ts"],
     darkMode: "class",
     theme: {
         extend: {}
