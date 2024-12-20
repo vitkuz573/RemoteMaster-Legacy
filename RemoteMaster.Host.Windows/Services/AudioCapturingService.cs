6f6e60e36fa07f31b71c2fa0a18afec6e270aba7
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-12-20 11:51:51 +0500

    UPD

--- RemoteMaster.Host.Windows/Services/AudioCapturingService.cs
+++ RemoteMaster.Host.Windows/Services/AudioCapturingService.cs
@@ -9,93 +9,58 @@ using RemoteMaster.Host.Core.Abstractions;
 
 namespace RemoteMaster.Host.Windows.Services;
 
-public class AudioCapturingService : IAudioCapturingService
+public class AudioCapturingService(ILogger<AudioCapturingService> logger) : IAudioCapturingService
 {
-    private readonly ILogger<AudioCapturingService> _logger;
-
-    private readonly WasapiLoopbackCapture _capture;
+    private readonly WasapiLoopbackCapture _capture = new();
     private readonly ConcurrentQueue<byte[]> _audioBuffer = new();
     private bool _isRecordingStarted;
-
-    public AudioCapturingService(ILogger<AudioCapturingService> logger)
-    {
-        _logger = logger;
-
-        _capture = new WasapiLoopbackCapture();
-
-        var waveFormat = _capture.WaveFormat;
-
-        _logger.LogInformation($"WaveFormat encoding: {_capture.WaveFormat.Encoding}");
-        _logger.LogInformation($"Audio capture initialized: {waveFormat.SampleRate} Hz, {waveFormat.BitsPerSample}-bit, {waveFormat.Channels} channels");
-    }
 
     public void StartRecording()
     {
         if (_isRecordingStarted)
         {
-            _logger.LogWarning("Recording already started.");
-
             return;
         }
 
         _capture.DataAvailable += Capture_DataAvailable;
         _capture.StartRecording();
         _isRecordingStarted = true;
-
-        _logger.LogInformation("Audio recording started.");
     }
 
     public void StopRecording()
     {
         if (!_isRecordingStarted)
         {
-            _logger.LogWarning("Recording has not started.");
             return;
         }
 
         _capture.StopRecording();
         _capture.DataAvailable -= Capture_DataAvailable;
         _isRecordingStarted = false;
-
-        _logger.LogInformation("Audio recording stopped.");
     }
 
     private void Capture_DataAvailable(object? sender, WaveInEventArgs e)
     {
         try
         {
-            _logger.LogInformation($"Captured {e.BytesRecorded} bytes of audio data.");
-
             var buffer = new byte[e.BytesRecorded];
             Array.Copy(e.Buffer, buffer, e.BytesRecorded);
-
-            var waveFormat = _capture.WaveFormat;
-
-            _logger.LogInformation($"Captured data: {waveFormat.SampleRate} Hz, {waveFormat.BitsPerSample}-bit, {waveFormat.Channels} channels");
 
             _audioBuffer.Enqueue(buffer);
         }
         catch (Exception ex)
         {
-            _logger.LogError($"Error while capturing audio: {ex.Message}");
+            logger.LogError($"Error while capturing audio: {ex.Message}");
         }
     }
 
     public byte[]? GetNextAudioChunk()
     {
-        var chunk = _audioBuffer.TryDequeue(out var data) ? data : null;
-
-        _logger.LogInformation(chunk != null
-            ? $"Extracted {chunk.Length} bytes of audio data from the buffer."
-            : "No audio data available in the buffer.");
-
-        return chunk;
+        return _audioBuffer.TryDequeue(out var data) ? data : null;
     }
 
     public void Dispose()
     {
-        _logger.LogInformation("Disposing audio capturing service.");
-
         if (_isRecordingStarted)
         {
             StopRecording();
@@ -104,7 +69,5 @@ public class AudioCapturingService : IAudioCapturingService
         _capture.Dispose();
 
         _audioBuffer.Clear();
-
-        _logger.LogInformation("Audio buffer cleared.");
     }
 }
