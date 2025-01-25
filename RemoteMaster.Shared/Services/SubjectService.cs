b478bb7a31c6cd25a50017a8b30cdd9cafeccdf2
Vitaly <vitkuz573@gmail.com>
2025-01-25 15:44:54 +0500

    UPD

--- RemoteMaster.Shared/Services/SubjectService.cs
+++ RemoteMaster.Shared/Services/SubjectService.cs
@@ -1,5 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
 
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Logging;
