42c678706f4cffdeb528061c458eecafd708d550
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-01-31 08:49:32 +0500

    UPD

---
+++ RemoteMaster.Host.Linux/Program.cs
@@ -0,0 +1,5 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+Console.WriteLine("Hello, World!");
