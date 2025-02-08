a6b76a61ab672076ac0ee956b0962a98798ea5c3
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-02-08 08:57:09 +0500

    Improve command line retrieval using efficient byte processing and modern C# features

--- RemoteMaster.Host.Linux/Services/CommandLineProvider.cs
+++ RemoteMaster.Host.Linux/Services/CommandLineProvider.cs
@@ -3,6 +3,7 @@
 // Licensed under the GNU Affero General Public License v3.0.
 
 using System.IO.Abstractions;
+using System.Text;
 using RemoteMaster.Host.Core.Abstractions;
 
 namespace RemoteMaster.Host.Linux.Services;
@@ -17,22 +18,45 @@ public class CommandLineProvider(IFileSystem fileSystem) : ICommandLineProvider
 
         if (!fileSystem.File.Exists(cmdlinePath))
         {
-            throw new InvalidOperationException($"The command-line file for process with ID {process.Id} does not exist.");
+            throw new InvalidOperationException($"The command line file for process with ID {process.Id} does not exist at '{cmdlinePath}'.");
         }
 
         try
         {
-            var cmdline = fileSystem.File.ReadAllText(cmdlinePath);
+            var bytes = fileSystem.File.ReadAllBytes(cmdlinePath);
+            
+            if (bytes.Length == 0)
+            {
+                return [];
+            }
 
-            return string.IsNullOrEmpty(cmdline) ? [] : cmdline.Split('\0', StringSplitOptions.RemoveEmptyEntries);
+            var arguments = new List<string>(capacity: 8);
+            ReadOnlySpan<byte> span = bytes.AsSpan();
+            var start = 0;
+            
+            for (var i = 0; i <= span.Length; i++)
+            {
+                if (i == span.Length || span[i] == 0)
+                {
+                    if (i > start)
+                    {
+                        var argument = Encoding.UTF8.GetString(span[start..i]);
+                        arguments.Add(argument);
+                    }
+
+                    start = i + 1;
+                }
+            }
+
+            return [.. arguments];
         }
-        catch (UnauthorizedAccessException)
+        catch (UnauthorizedAccessException ex)
         {
-            throw new InvalidOperationException($"Insufficient permissions to read the file {cmdlinePath}.");
+            throw new InvalidOperationException($"Insufficient permissions to read the file '{cmdlinePath}'.", ex);
         }
         catch (Exception ex)
         {
-            throw new InvalidOperationException($"Failed to read the command line for process with ID {process.Id}.", ex);
+            throw new InvalidOperationException($"Failed to retrieve the command line for process with ID {process.Id}.", ex);
         }
     }
 }
