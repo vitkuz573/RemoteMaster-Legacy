9d76d48c01c47f39dd2507e96010cc09122fbac2
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-02-09 08:24:10 +0500

    UPD

---
+++ RemoteMaster.Host.Linux.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © 2023 Vitaly Kuzyaev. All rights reserved.
+// This file is part of the RemoteMaster project.
+// Licensed under the GNU Affero General Public License v3.0.
+
+namespace RemoteMaster.Host.Linux.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
