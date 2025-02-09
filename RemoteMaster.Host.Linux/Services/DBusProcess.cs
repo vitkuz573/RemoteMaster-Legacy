fd4706679203f3723188861dbf148b5d13954c91
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-02-09 16:06:02 +0500

    UPD

--- RemoteMaster.Host.Linux/Services/DBusProcess.cs
+++ RemoteMaster.Host.Linux/Services/DBusProcess.cs
@@ -86,12 +86,12 @@ public class DBusProcess(INativeProcessOptions processOptions, IProcessService p
 
         _unitJobPath = job;
 
-        var unitPathStr = "/org/freedesktop/systemd1/unit/" +
-                          unitName.ToLower()
-                              .Replace("-", "_2d")
-                              .Replace(".", "_2e");
+        var unitBasePath = "/org/freedesktop/systemd1/unit/";
+        var unitPathName = unitName.ToLower()
+            .Replace("-", "_2d")
+            .Replace(".", "_2e");
 
-        _unitPath = new ObjectPath(unitPathStr);
+        _unitPath = new ObjectPath($"{unitBasePath}{unitPathName}");
 
         await Task.Delay(5000);
 
