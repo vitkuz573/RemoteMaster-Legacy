248cc8ecffcd8a0b283c2bb62221cf1b985c28c6
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-02-17 20:12:16 +0500

    UPD

--- RemoteMaster.Server/Attributes/CustomMinLengthAttribute.cs
+++ RemoteMaster.Server/Attributes/CustomMinLengthAttribute.cs
@@ -2,21 +2,96 @@
 // This file is part of the RemoteMaster project.
 // Licensed under the GNU Affero General Public License v3.0.
 
+// Temporary solution until https://github.com/dotnet/runtime/issues/112111 is resolved.
+
+using System.Collections;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace RemoteMaster.Server.Attributes;
 
-public class CustomMinLengthAttribute(int length) : ValidationAttribute
+/// <summary>
+/// Specifies the minimum length of collection or string data allowed in a property.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class CustomMinLengthAttribute : ValidationAttribute
 {
-    public int Length { get; } = length;
+    /// <summary>
+    /// Gets the minimum allowable length.
+    /// </summary>
+    public int Length { get; }
 
-    public override bool IsValid(object? value)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CustomMinLengthAttribute"/> class.
+    /// </summary>
+    /// <param name="length">
+    /// The minimum allowable length of collection or string data.
+    /// Value must be greater than or equal to zero.
+    /// </param>
+    public CustomMinLengthAttribute(int length) : base("The field {0} must be a string or collection type with a minimum length of {1}.")
     {
-        if (value is string str)
+        if (length < 0)
         {
-            return str.Length >= Length;
+            throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be negative.");
         }
 
-        return false;
+        Length = length;
+    }
+
+    /// <summary>
+    /// Determines whether the specified value is valid.
+    /// Null values are considered valid. To enforce non-null values, use the <see cref="RequiredAttribute"/>.
+    /// </summary>
+    /// <param name="value">The value to validate.</param>
+    /// <returns>
+    /// true if the value is null or its length is greater than or equal to the specified minimum; otherwise, false.
+    /// </returns>
+    public override bool IsValid(object? value)
+    {
+        if (value is null)
+        {
+            return true;
+        }
+
+        int actualLength;
+
+        if (value is string str)
+        {
+            actualLength = str.Length;
+        }
+        else if (value is ICollection collection)
+        {
+            actualLength = collection.Count;
+        }
+        else if (value is IEnumerable enumerable)
+        {
+            actualLength = 0;
+
+            foreach (var _ in enumerable)
+            {
+                actualLength++;
+
+                if (actualLength >= Length)
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            throw new InvalidCastException(string.Format(CultureInfo.CurrentCulture, "The type '{0}' is not a valid type for CustomMinLengthAttribute. It must be a string or a collection type.", value.GetType()));
+        }
+
+        return actualLength >= Length;
+    }
+
+    /// <summary>
+    /// Applies formatting to a specified error message.
+    /// </summary>
+    /// <param name="name">The name to include in the formatted string.</param>
+    /// <returns>A localized string to describe the error.</returns>
+    public override string FormatErrorMessage(string name)
+    {
+        return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, Length);
     }
 }
